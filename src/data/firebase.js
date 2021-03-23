4dd86421ba25aca90f8bc370ead1dd85b2f58606
Bereket Semagn <semagnbereket@gmail.com>
2021-03-23 12:55:36 -0400

    added REACT_APP to env config

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 require("dotenv").config();
 
 const config = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectID: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export function login(provider) {
