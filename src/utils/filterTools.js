3e82042df47f6a8b4b52bdd21ddc035cb97d428c
Bereket Semagn <semagnbereket@gmail.com>
2021-03-25 14:17:19 -0400

    Revert "Enable search and separation of concerns (2)"

--- src/utils/filterTools.js
+++ src/utils/filterTools.js
@@ -2,13 +2,10 @@
 
 /* 
 
-{/* 
 for the _filterToolsByCategory_, it takes param tools and currCategory
 The [tools] reference the search space i.e the array of objects from which the tools have to be filtered
 The [currCategory] reference the category by which one want's to search a tool
-
 */
-
 
 function filterToolsByCategory(tools, currCategory) {
   const toolsByCategory =
@@ -18,26 +15,19 @@ function filterToolsByCategory(tools, currCategory) {
   return toolsByCategory;
 }
 
-{
 /* 
 for _searchTools_, it takes a searchQuery and a searchSpace
 The [searchQuery] is any string by which to search the tool -> could be name, description or even category
 The searchSpace param is the space(values ) from which to search the tools
 */
-}
 
 function searchTools(searchQuery, searchSpace) {
-    // filter? 
-    const search = new RegExp(searchQuery)
-    const toolsBySearch = searchSpace.filter(tool => {
-    let match = ((tool.name + tool.desc + tool.price + tool.owner).toLowerCase() + searchSpace.indexOf(tool))
-    let unitMatchResult = match.match(search) ? match.match(search).input : ""
-    let toolNameArr = unitMatchResult.split("")
-    let toolIndex = toolNameArr[toolNameArr.length - 1]
-    // console.log(toolIndex)
-    return unitMatchResult[0]
-    })
-    return toolsBySearch
+  const toolsBySearch = searchSpace.filter(tool =>
+    (tool.name, tool.desc, tool.pricing, tool.category).includes(
+      searchQuery.toLowerCase()
+    )
+  );
+  return toolsBySearch;
 }
 
 export { filterToolsByCategory, searchTools };
