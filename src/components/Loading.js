6633a6e96aaf66b3f11554ce726d2d37913df122
Bereket Semagn <semagnbereket@gmail.com>
2021-03-25 09:42:12 -0400

    loading message in center

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,17 @@
 const Loading = ({ message }) => {
   return (
     <>
-      <p className="center">
+      <p className="loading">
         <strong>loading {message}...</strong>
       </p>
+
+      <style jsx>
+        {`
+          .loading {
+            text-align: center; 
+          }
+        `}
+      </style>
     </>
   );
 };
