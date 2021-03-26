03447f67f175af24e044515821e585c5c600abeb
Mudrank <70035508+Mudrank@users.noreply.github.com>
2021-03-26 12:23:04 +0530

    Created translation for Hindi

---
+++ public/locales/IN/translation.js
@@ -0,0 +1,26 @@
+{
+  "common": {},
+  "home": {
+    "browseProjects": "{{total}}+ ओपन सोर्स प्रोजेक्ट ब्राउज़ करें।",
+    "searchProjectsPlaceholder": "प्रोजेक्ट खोजें ...",
+    "all": "All"
+  },
+  "new": {
+    "addRepoHeader": "कोड श्रद्धांजलि में एक नया रेपो जोड़ें",
+    "addRepo": "रेपो जोड़ें",
+    "addRepoPlaceholder": "पूर्ण  यूआरएल (ex. https://github.com/heybereket/oasis)",
+    "signInToSubmit": "सबमिट करने के लिए साइन इन करें"
+  },
+  "header": {
+    "submit" : "भेजे",
+    "signInUp": "साइन अप/में",
+    "signOut": "साइन आउट"
+  },
+  "footer": {
+    "oasisBy": "ओएसिस द्वारा",
+    "followUsOn": "follow us on",
+    "twitter": "twitter",
+    "openSourceOn": "ओपन-सोर्स :",
+    "github": "github"
+  }
+} 
