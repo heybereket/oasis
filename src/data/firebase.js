ef5a1457911a32f569e0664ad02495f81eacb4c0
Bereket Semagn <semagnbereket@gmail.com>
2021-03-29 17:50:46 -0400

    reload after sign in/up (put back in)

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -43,7 +43,7 @@ export function login(provider) {
       const projectRef = db.collection("users").doc(data.login)
       projectRef.set(userData)
 
-      
+      window.location.reload()
   })
 }
 
