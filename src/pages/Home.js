118362236611e3930c4f452427e3a06afbc1f4e8
Jabo <joelvincent.work@gmail.com>
2021-04-01 03:26:22 +0800

    :art: code refactor (#56)
    Improved code organization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ import '../style/App.css'
 import { colours } from '../lib/constants.js'
 import BackToTop from '../components/BackToTop'
 import { useTranslation, Trans } from 'react-i18next'
+import RepositoryList from '../components/RepositoryList'
 
 const Button = ({ category, isActive, onClick }) => {
   return (
@@ -143,115 +144,7 @@ const Home = () => {
       )}
 
       <div className="repos">
-        {isLoading ? (
-          <Loading message="repos" />
-        ) : (
-          <div className="repos">
-            {projectsFilteredByCategoryAndSearchQuery.map((project, index) => (
-              <Link
-                key={project.url + index}
-                to={`/r/${project.owner}/${project.name}`}
-                rel="noreferrer"
-              >
-                <div className="repo">
-                  {project.fork === true && (
-                    <div
-                      className="fork-icon"
-                      title={`${project.full_name} is a forked repository`}
-                    >
-                      <svg
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        width="20"
-                        height="20"
-                        aria-hidden="true"
-                        fill="white"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                        ></path>
-                      </svg>
-                    </div>
-                  )}
-
-                  <img
-                    alt={`${project.owner.toLowerCase()}'s logo`}
-                    className="display"
-                    src={project.avatar}
-                    title={project.owner}
-                  />
-                  <p key={index}>
-                    <span className="owner">{project.owner}</span>/
-                    <span className="name">{project.name}</span>
-                  </p>
-
-                  {project.desc != null && <small>{project.desc}</small>}
-
-                  {project.desc === null && <small>No description.</small>}
-
-                  <div className="category-wrapper">
-                    {project.language in colours && (
-                      <button className="language">
-                        <svg viewBox="0 0 80 80" width="10" height="10">
-                          <circle
-                            style={{ fill: colours[project.language] }}
-                            className="circle"
-                            cx="40"
-                            cy="40"
-                            r="38"
-                          />
-                        </svg>
-                        &nbsp;
-                        {project.language}
-                      </button>
-                    )}
-
-                    {!(project.language in colours) && project.language != null && (
-                      <button className="language">
-                        <svg viewBox="0 0 80 80" width="10" height="10">
-                          <circle
-                            style={{ fill: '#fff' }}
-                            className="circle"
-                            cx="40"
-                            cy="40"
-                            r="38"
-                          />
-                        </svg>
-                        &nbsp;
-                        {project.language}
-                      </button>
-                    )}
-
-                    {project.language === null && (
-                      <button className="language">
-                        <svg viewBox="0 0 80 80" width="10" height="10">
-                          <circle
-                            style={{ fill: '#fff' }}
-                            className="circle"
-                            cx="40"
-                            cy="40"
-                            r="38"
-                          />
-                        </svg>
-                        &nbsp; N/A
-                      </button>
-                    )}
-
-                    {project.issues > 1000 ? (
-                      <button className="issues">🚨 1k+ issues</button>
-                    ) : (
-                      <button className="issues">🚨 {project.issues} issues</button>
-                    )}
-
-                    <br />
-                    <button className="stars">⭐ {formatNumber(project.stars)} stars</button>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
-        )}
+        {isLoading ? <Loading message="repos" /> : <RepositoryList repositories={projectsFilteredByCategoryAndSearchQuery} className="repos"/>}
       </div>
       <BackToTop />
       <Footer />
