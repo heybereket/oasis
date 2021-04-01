16b77413d8fa232f6f60a1082b5099c78e09619e
Bereket Semagn <semagnbereket@gmail.com>
2021-03-31 21:34:47 -0400

    starting redesign...

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-import Head from 'next/head'
-import Card from '../components/card.js'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="center flex-col min-h-screen px-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="center flex-col flex-1 py-20">
-        <h1 className="text-center m-0 text-7xl">
-          Oasis<a href="https://nextjs.org" className="text-blue-500">.sh</a>
-        </h1>
-      </main>
+    <div className="min-h-screen bg-gray-50 font-sans antialiased">
+      <Navbar />
     </div>
-  )
+  );
 }
