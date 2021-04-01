c8b6332de812141549b2bb3c577bdd146aa97664
Uthman Igbin <uthmanigbin@gmail.com>
2021-04-01 03:31:18 +0300

    Made 404 page more beautiful

--- src/components/InvalidPage.js
+++ src/components/InvalidPage.js
@@ -1,26 +1,105 @@
-import "../style/InvalidPage.css";
-import { Link } from "react-router-dom";
+import '../style/InvalidPage.css'
+import { Link } from 'react-router-dom'
 
 const InvalidPage = () => {
   return (
     <>
-      <main>
-        <h1 className="invalid-heading">404 😢</h1>
-        <span className="description">
-          <b>bummer!</b> the page you are looking for could not be found.
-        </span>
-        <br />
-        <div align="right">
-          <Link to="/">
-            <button>home / repos 🎉</button>
-          </Link>
-          <Link to="/new">
-            <button>submit a repo 🔥</button>
-          </Link>
-        </div>
+      <main className="container">
+        <div className="demo"></div>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">4</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <span className="particle">0</span>
+        <article className="content">
+          <p>Damnit stranger,</p>
+          <p>
+            You got lost in the <strong>404</strong> galaxy.
+          </p>
+          <p>
+            <Link to="/">
+              <button>home / repos 🎉</button>
+            </Link>
+          </p>
+        </article>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default InvalidPage;
+export default InvalidPage
