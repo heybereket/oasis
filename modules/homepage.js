c261a5ed1615d183a22092c473ca59ee3bb44b53
Uthman <uthmanigbin@gmail.com>
2021-04-03 03:32:06 +0300

    Next (#70)

--- modules/homepage.js
+++ modules/homepage.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Appbar from '../components/card'
+import React from "react";
+//import Appbar from '../components/card'
 
-export default function Homepage () {
+export default function Homepage() {
   return (
-    <div>
-      <Appbar />
-      <h1 className={'text-4xl font-bold text-center mt-16'}>Coming Soon!</h1>
-      <p className={'text-center font-mono text-sm mt-2 text-gray-700'}>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <h1 className={"text-4xl font-bold text-center mt-16 text-black"}>
+        Coming Soon!
+      </h1>
+      <p className={"text-center font-mono text-sm mt-2 text-gray-800"}>
         Note: This is a placeholder for when we add an actual homescreen.
       </p>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
+import Homepage from "../modules/homepage";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 font-sans antialiased">
       <Navbar />
+      <Homepage />
     </div>
   );
 }
