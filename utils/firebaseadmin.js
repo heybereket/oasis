9829885b36e0b4e4dc3241304c7fb7e3d061e510
Bereket Semagn <semagnbereket@gmail.com>
2021-04-10 15:08:57 -0400

    checking if private key is not undefined

--- utils/firebaseadmin.js
+++ utils/firebaseadmin.js
@@ -4,7 +4,7 @@ export const serviceAccount = {
   type: 'service_account',
   project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replaceAll('\\n', '\n'),
+  private_key: process.env.PRIVATE_KEY,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
@@ -13,8 +13,8 @@ export const serviceAccount = {
   client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
 };
 
-if (process.env.PRIVATE_KEY === undefined) {
-  throw new Error('PRIVATE_KEY is undefined')
+if (process.env.PRIVATE_KEY !== undefined) {
+  process.env.PRIVATE_KEY.replaceAll('\\n', '\n')
 }
 
 export default async function getFirebaseAdmin() {
