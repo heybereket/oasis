feba92eaf06132a22d63306dd2fa72aa3386b561
coderinblack <kevinlu.email@gmail.com>
2021-04-12 13:14:40 -0700

    Fix firebase import error

--- modules/landing/HomePage.tsx
+++ modules/landing/HomePage.tsx
@@ -1,19 +1,21 @@
-import { MarkGithubIcon } from '@primer/octicons-react';
-import firebase from 'firebase';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Button } from '../../components/Button';
-import { Navbar } from '../../components/Navbar';
-import { login } from '../../lib/auth';
+import { MarkGithubIcon } from "@primer/octicons-react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Button } from "../../components/Button";
+import { Navbar } from "../../components/Navbar";
+import { login } from "../../lib/auth";
 
 export const HomePage: React.FC = () => {
   const [user, loading, error] = useAuthState(firebase.auth());
 
   useEffect(() => {
     if (user) {
-      console.log('Logged in');
+      console.log("Logged in");
     }
   }, []);
 
@@ -28,23 +30,31 @@ export const HomePage: React.FC = () => {
             The newest home for developers.
           </h1>
           <p className="text-gray-300 text-base sm:text-lg md:text-xl mt-3">
-            <span className="text-gray-200 font-medium">Introducing Oasis</span>{' '}
+            <span className="text-gray-200 font-medium">Introducing Oasis</span>{" "}
             — your developer corner of the internet.
           </p>
           <div className="flex flex-col sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-9">
-            <Button
-              className="hover:bg-[#5C91FC]"
-              onClick={login}
-            >
+            <Button className="hover:bg-[#5C91FC]" onClick={login}>
               <MarkGithubIcon /> &nbsp; Log in with GitHub
             </Button>
-            <Button className="hover:bg-gray-500" color="gray">Log in Anonymously</Button>
+            <Button className="hover:bg-gray-500" color="gray">
+              Log in Anonymously
+            </Button>
           </div>
           <p className="text-gray-300 text-base sm:text-xs md:small mt-3">
-            By logging in, you accept our 
+            By logging in, you accept our{" "}
             <Link href="/privacy">
-               <a className="text-primary-light hover:underline">Privacy Policy</a></Link>
-             and <Link href="/terms"><a className="text-primary-light hover:underline">Terms of Service</a></Link> </p>
+              <a className="text-primary-light hover:underline">
+                Privacy Policy
+              </a>
+            </Link>
+            and{" "}
+            <Link href="/terms">
+              <a className="text-primary-light hover:underline">
+                Terms of Service
+              </a>
+            </Link>
+          </p>
         </div>
       </div>
       <div className="hidden md:flex absolute bottom-0 z-0 w-full justify-center">
--- modules/landing/NotFoundPage.tsx
+++ modules/landing/NotFoundPage.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export const NotFoundPage: React.FC = () => {
-  return (
-    <div>
-
-    </div>
-  );
+  return <div></div>;
 };
