ff9b332d26c47cbfcb6c54bbba43ccead83d140b
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-04-12 18:00:22 -0700

    well the first time didn't work so well so i reworked the positioning and it works better now

--- modules/error/NotFoundPage.tsx
+++ modules/error/NotFoundPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export const NotFoundPage: React.FC = () => {
-  return <div className="flex justify-center">
-      <h1 className="absolute top-1/3">404</h1>
-      <p className="absolute bottom-[52.4%] text-gray-300 text-lg">Silly you, that page doesn’t exist</p>
+  return <div className="grid justify-items-center mb-4">
+      <div className="absolute top-1/3 text-center">
+      <h1>404</h1>
+      <p className="text-gray-300 text-lg mt-4 ">Silly you, that page doesn’t exist</p>
+      </div>
   </div>;
 };
