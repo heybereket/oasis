fc58fb55eebd945069e895604962e65da9e2973c
Bereket Semagn <semagnbereket@gmail.com>
2021-04-13 19:09:29 -0400

    trigger build

--- scripts/electron-deploy.sh
+++ scripts/electron-deploy.sh
@@ -1,2 +1,17 @@
+# Add all files 
 git add -A
+
+# Make a commit
 git commit -m "trigger build"
+
+# Change branch to prod
+git checkout prod
+
+# Merge staging into prod
+git merge staging
+
+# Push all changes to GitHub
+git push origin prod
+
+# Go back to staging
+git checkout staging
