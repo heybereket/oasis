209288f7f35e66b76398699e977a4cb3b80db0c4
tacheometry <39647014+tacheometry@users.noreply.github.com>
2021-04-13 17:59:33 +0300

    fix 404 (#113)

--- modules/error/NotFoundPage.tsx
+++ modules/error/NotFoundPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export const NotFoundPage: React.FC = () => {
-
   return (
-    <div className="flex flex-col items-center text-center mt-80 h-screen">
-      <h1>404</h1>
-      <p className="text-gray-300 text-lg mt-4">
-        Silly you, that page doesn’t exist
-      </p>
+    <div className="flex h-full">
+      <div className="m-auto text-center">
+        <h1>404</h1>
+        <p className="text-gray-300 text-lg mt-4">
+          Silly you, that page doesn’t exist
+        </p>
+      </div>
     </div>
   );
-
 };
