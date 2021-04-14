16d4cdacdc08f147a2fd9135d4e3844388ebd315
Bereket Semagn <semagnbereket@gmail.com>
2021-04-14 08:15:50 -0400

    trigger build

--- scripts/trigger.sh
+++ scripts/trigger.sh
@@ -1,17 +1,22 @@
-# Add all files 
-git add -A
+read -p "Deploy to production (y/n)? " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # Add all files 
+  git add -A
 
-# Make a commit
-git commit -m "trigger build"
+  # Make a commit
+  git commit -m "trigger build"
 
-# Change branch to prod
-git checkout prod
+  # Change branch to prod
+  git checkout prod
 
-# Merge staging into prod
-git merge staging
+  # Merge staging into prod
+  git merge staging
 
-# Push all changes to GitHub
-git push origin prod
+  # Push all changes to GitHub
+  git push origin prod
 
-# Go back to staging
-git checkout staging
+  # Go back to staging
+  git checkout staging
+fi
