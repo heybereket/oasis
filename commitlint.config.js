fb645fe49b995fe6555ada2076f9ee31a6991533
Bereket Semagn <semagnbereket@gmail.com>
2021-04-14 19:21:18 -0400

    added commitlint config file

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "global",
+        "web", 
+        "desktop",
+        "api"
+      ],
+    ],
+  },
+};
