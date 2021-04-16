79c4390641969d5a199e706a6e72168747e83ee3
Ongshu777 <51079288+Ongshu777@users.noreply.github.com>
2021-04-16 21:30:49 +0100

    Move graphql logic to packages/api

--- packages/api/src/resolvers.ts
+++ packages/api/src/resolvers.ts
@@ -1,11 +1,11 @@
 import { IResolvers } from 'graphql-tools';
-import getFirebaseAdmin from '../../utils/firebase-admin';
+import getFirebaseAdmin from './firebase-admin';
 
 export const resolvers: IResolvers = {
   Query: {
     getUsers: async () => {
-      var db = await admindb();
-      var users = await db.collection('users').get();
+      const db = await admindb();
+      const users = await db.collection('users').get();
       return users.docs.map((doc) => doc.data());
     },
   },
--- packages/web/src/pages/api/graphql.ts
+++ packages/web/src/pages/api/graphql.ts
@@ -1,16 +1,3 @@
-import { ApolloServer } from 'apollo-server-micro';
-import { typeDefs } from './schemas';
-import { resolvers } from './resolvers';
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+import { apolloServer } from '@oasis/api';
 
 export default apolloServer.createHandler({ path: '/api/graphql' });
