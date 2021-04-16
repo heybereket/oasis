9609d36b492bda4af47b5ad12fa0322a5a210cf7
vishy-dev <agarwal.vrishank@gmail.com>
2021-04-16 19:39:57 +0100

    optimisations

--- packages/web/src/pages/api/resolvers.ts
+++ packages/web/src/pages/api/resolvers.ts
@@ -4,10 +4,14 @@ import getFirebaseAdmin from '../../utils/firebase-admin';
 export const resolvers: IResolvers = {
   Query: {
     getUsers: async () => {
-      const admin = await getFirebaseAdmin();
-      const db = admin.firestore();
+      var db = await admindb();
       var users = await db.collection('users').get();
       return users.docs.map((doc) => doc.data());
     },
   },
 };
+
+async function admindb(): Promise<FirebaseFirestore.Firestore> {
+  const admin = await getFirebaseAdmin();
+  return admin.firestore();
+}
