770e0a73fd9a82f07fbf2e1a4592841c92987479
Bereket Semagn <semagnbereket@gmail.com>
2021-04-15 20:35:17 -0400

    seo component

---
+++ packages/web/src/components/SEO.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+interface SEOProps {
+  title: string;
+}
+
+export const SEO: React.FC<SEOProps> = ({title}) => {
+  return (
+    <Head>
+      <title>{title}</title>
+    </Head>
+  );
+}
