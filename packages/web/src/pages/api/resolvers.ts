05af421410a0222dd16d25d69cd916a9bff2acef
vishy-dev <agarwal.vrishank@gmail.com>
2021-04-16 19:30:31 +0100

    gql fixes

--- packages/web/src/pages/api/resolvers.ts
+++ packages/web/src/pages/api/resolvers.ts
@@ -1,14 +1,13 @@
+import { IResolvers } from 'graphql-tools';
 import getFirebaseAdmin from '../../utils/firebase-admin';
 
-export async function resolvers() {
-  const admin = await getFirebaseAdmin();
-  const db = admin.firestore();
-  return {
-    Query: {
-      getUsers: async () => {
-        var users = await db.collection('users').get();
-        return users.docs.map((doc) => doc.data());
-      },
+export const resolvers: IResolvers = {
+  Query: {
+    getUsers: async () => {
+      const admin = await getFirebaseAdmin();
+      const db = admin.firestore();
+      var users = await db.collection('users').get();
+      return users.docs.map((doc) => doc.data());
     },
-  };
-}
+  },
+};
