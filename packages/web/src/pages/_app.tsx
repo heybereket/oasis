3a4865ba8b68847cb0d41d6002360513949d3432
Mudrank Gupta <70035508+Mudrank@users.noreply.github.com>
2021-04-17 16:36:14 +0530

    Added icons (FAVICON)
    this a follow of pr I made just before this in assets

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -21,6 +21,12 @@ function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0"
         />
+           <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
     </>
