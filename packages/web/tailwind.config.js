c2af8c8e3d039326deb0d2f4a0faf78e0de46987
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-04-17 12:28:55 -0700

    made responsive navbar

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -34,6 +34,37 @@ module.exports = {
         lighter: 'var(--color-primary-lighter)',
       },
     },
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+            '0%': {
+                opacity: '0',
+                transform: 'translateY(-10px)'
+            },
+            '100%': {
+                opacity: '1',
+                transform: 'translateY(0)'
+            },
+        },
+        'fade-out-up': {
+          '100%': {
+              opacity: '0',
+              visibility: 'hidden',
+              transform: 'translateY(-10px)'
+          },
+          '0%': {
+              opacity: '1',
+              transform: 'translateY(0)'
+          },
+      },
+        
+        
+    },
+    animation: {
+        'fade-in-down': 'fade-in-down 0.1s ease-out',
+        'fade-out-up': 'fade-out-up 0.1s ease-out'
+    }
+    }
   },
   variants: {
     extend: {},
