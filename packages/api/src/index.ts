3a3eeaaeaa17b6b9b6352e972fc5ba0ea51ea57f
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-17 07:29:02 +0100

    Fix environment variables

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,8 @@
-import { ApolloServer } from 'apollo-server-micro';
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
+import { config } from "dotenv";
+config();
+import { ApolloServer } from "apollo-server-micro";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
 
 export const apolloServer = new ApolloServer({
   typeDefs,
