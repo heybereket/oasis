392b8247d56c40664fcc1db51e69993f510d8f77
Samar <samar.mohan@gmail.com>
2021-04-17 10:45:18 -0500

    Add GitHub link to navbar
    Discord link will not be implemented yet.

--- packages/web/src/components/Navbar.tsx
+++ packages/web/src/components/Navbar.tsx
@@ -6,12 +6,15 @@ export const Navbar: React.FC = () => {
       <img src="/static/oasis-logo.png" alt="Oasis Logo" className="w-32" />
       <ul className="flex items-center space-x-6 md:space-x-10 text-lg text-gray-300">
         <li>
-          <a href="#" className="hover:text-gray-200">
+          <a
+            href="https://github.com/oasis-sh/oasis/"
+            className="hover:text-gray-200"
+          >
             GitHub
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-200">
+          <a href="/" className="hover:text-gray-200">
             Discord
           </a>
         </li>
