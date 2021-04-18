bb4bde0b29fb82b739b93c677e8a156948d91b9a
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-18 12:31:46 +0100

    Ok I give up

--- packages/api/src/resolvers.ts
+++ packages/api/src/resolvers.ts
@@ -1,12 +1,8 @@
-import { join } from "path";
 import { mergeResolvers } from "@graphql-tools/merge";
-import { loadFilesSync } from "@graphql-tools/load-files";
 
-const dirname = process.env.PROJECT_ROOT;
-const resolversArray = loadFilesSync(
-  join(dirname, "/packages/api/dist/modules/**/resolvers.js")
-);
+import repo from "./modules/repo/resolvers";
+import user from "./modules/user/resolvers";
 
-console.log(resolversArray);
+const resolversArray = [repo, user];
 
 export default mergeResolvers(resolversArray);
--- packages/api/src/typeDefs.ts
+++ packages/api/src/typeDefs.ts
@@ -1,15 +1,11 @@
-import { join } from "path";
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import { loadFilesSync } from "@graphql-tools/load-files";
 import { writeFileSync } from "fs";
 import { print } from "graphql";
 
-const dirname = process.env.PROJECT_ROOT;
-const typeDefsArray = loadFilesSync(
-  join(dirname, "/packages/api/dist/modules/**/typeDefs.js")
-);
+import repo from "./modules/repo/typeDefs";
+import user from "./modules/user/typeDefs";
 
-console.log(typeDefsArray);
+const typeDefsArray = [repo, user];
 
 const typeDefs = mergeTypeDefs(typeDefsArray);
 export default typeDefs;
