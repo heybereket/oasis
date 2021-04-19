de4ee157132e93fd20cb53dd9ae998e31a642fbf
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-19 19:43:30 +0100

    idk what changes I made for this one

--- packages/api/src/modules/repo/resolvers.ts
+++ packages/api/src/modules/repo/resolvers.ts
@@ -1,12 +1,25 @@
 import { IResolvers } from "graphql-tools";
 import { adminDB } from "../../utils/admin-db";
 
+const getReposCollection = async () => {
+  const db = await adminDB();
+  return db.collection("repos");
+};
+
 const resolvers: IResolvers = {
+  User: {
+    repos: () => [],
+  },
   Query: {
     allRepos: async () => {
-      const db = await adminDB();
-      const users = await db.collection("repos").get();
+      const collection = await getReposCollection();
+      const users = await collection.get();
       return users.docs.map((doc) => doc.data());
+    },
+    getRepo: async (_, { id }: { id: string }) => {
+      const collection = await getReposCollection();
+      const doc = await collection.doc(id).get();
+      return doc.data();
     },
   },
 };
