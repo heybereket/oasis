15a7310c984bcd4eb6949c0fd21abada2cac8317
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-19 19:46:16 +0100

    API Auth

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,32 @@
 import { config } from "dotenv";
 config();
 
-import { ApolloServer } from "apollo-server-micro";
+import { ApolloError, ApolloServer } from "apollo-server-micro";
 import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
+import { IncomingMessage } from "http";
+
+const accessDeniedMsg = "Damn. You kinda don't have access to this api";
 
 export const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  context({ req }: { req: IncomingMessage }) {
+    const header = req.headers.authorization || "";
+    const [, token] = header.split(" ");
+
+    if (token !== process.env.API_ACCESS_TOKEN) {
+      throw new ApolloError(accessDeniedMsg);
+    }
+
+    return {};
+  },
+  formatError(err) {
+    if (err.message.includes(accessDeniedMsg)) {
+      return new Error(accessDeniedMsg);
+    }
+    return err;
+  },
   playground: true,
   introspection: true,
 });
