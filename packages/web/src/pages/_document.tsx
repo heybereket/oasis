b417729b9e7970186eb96642a393f298d5712974
Bereket Semagn <semagnbereket@gmail.com>
2021-04-22 09:30:29 -0400

    manifest

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -3,17 +3,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head>
-        <link rel="manifest" href="/manifest.json"/>
+      <Html lang="en-GB">
+      <Head>
+        <meta name="theme-color" content="#673ab7" />
+        <meta
+          name="Description"
+          content="an example of NextJS app with 100% accessible lighthouse score"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="static/img/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
       </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
     )
   }
 }
+
+
 
 export default MyDocument
