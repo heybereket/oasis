b5b3fec785bd063405ab70cefa55b36d3b28b4fb
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-22 18:18:32 +0100

    Basically delete everything

--- packages/api/src/utils/admin-db.ts
+++
@@ -1,6 +0,0 @@
-import getFirebaseAdmin from "./firebase-admin";
-
-export async function adminDB(): Promise<FirebaseFirestore.Firestore> {
-  const admin = await getFirebaseAdmin();
-  return admin.firestore();
-}
