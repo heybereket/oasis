4aeb32ef75b886e81526d05901eb694b30222b7c
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-23 06:51:30 +0100

    Create a build script

---
+++ packages/api/src/build.ts
@@ -0,0 +1,25 @@
+import { glob } from "glob";
+import { join } from "path";
+import { writeFileSync } from "fs";
+
+glob(join(__dirname, "./modules/**/resolvers/*.js"), (err, absolutes) => {
+  if (err) console.error("Error while glob! ", err);
+
+  const filenames = absolutes.map(
+    (filename) => "." + filename.slice(__dirname.length)
+  );
+
+  let count = 0;
+
+  const output =
+    filenames
+      .map((filename) => {
+        return `var R${count++} = require("${filename}").default;`;
+      })
+      .join("\n") +
+    `\n\nmodule.exports = [${Array(count)
+      .fill(0)
+      .map((_, i) => "R" + i)}]`;
+
+  writeFileSync("./dist/__resolvers.js", output);
+});
