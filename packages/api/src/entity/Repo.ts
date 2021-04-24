ff99439d608726accf83d7e77716da6083b52127
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-24 08:30:49 +0100

    Use `getRefData` util on Repo entity

--- packages/api/src/entity/Repo.ts
+++ packages/api/src/entity/Repo.ts
@@ -1,3 +1,4 @@
+import { getRefData } from "src/utils/getRefData";
 import { Field, ID, ObjectType, Root } from "type-graphql";
 import { BaseEntity, Entity } from "../connection";
 import User from "./User";
@@ -18,9 +19,6 @@ export default class Repo extends BaseEntity {
 
   @Field(() => User, { nullable: true })
   async owner(@Root() parent: Repo) {
-    if (!parent._owner) return null;
-
-    const snap = await parent._owner.get();
-    return { id: snap.id, ...snap.data() };
+    return parent._owner ? getRefData(parent._owner) : null;
   }
 }
