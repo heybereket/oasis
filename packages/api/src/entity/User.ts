04e0e876865011609f10efdb1b98bf9afc8b919f
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-24 09:16:00 +0100

    User -> Posts connection

--- packages/api/src/entity/User.ts
+++ packages/api/src/entity/User.ts
@@ -2,6 +2,7 @@ import { getRefData } from "../utils/getRefData";
 import { Field, ID, ObjectType, Root } from "type-graphql";
 import { BaseEntity, Entity } from "../connection";
 import Repo from "./Repo";
+import Post from "./Post";
 
 @ObjectType()
 @Entity("users")
@@ -19,11 +20,21 @@ export default class User extends BaseEntity {
   username: string;
 
   _repos: FirebaseFirestore.DocumentReference[];
+  _posts: FirebaseFirestore.DocumentReference[];
 
   @Field(() => [Repo], { nullable: true })
   async repos(@Root() parent: User) {
     if (!parent._repos) return;
 
     return Promise.all(parent._repos.map(getRefData));
+  }
+
+  @Field(() => [Post], { nullable: true })
+  async posts(@Root() parent: User) {
+    console.log(parent);
+
+    if (!parent._posts) return;
+
+    return Promise.all(parent._posts.map(getRefData));
   }
 }
