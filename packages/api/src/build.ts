1c15cfce1fbb9ece7ae041a585fe1fe70a8e1960
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-28 07:20:58 +0100

    Add build file

--- packages/api/src/build.ts
+++ packages/api/src/build.ts
@@ -1,26 +1,26 @@
-import { glob } from "glob";
-import { join } from "path";
-import { writeFileSync } from "fs";
-import { resolversPattern } from "./globPatterns";
+// import { glob } from "glob";
+// import { join } from "path";
+// import { writeFileSync } from "fs";
+// import { resolversPattern } from "./globs/globPatterns";
 
-glob(join(__dirname, resolversPattern), (err, absolutes) => {
-  if (err) console.error("Error while glob! ", err);
+// glob(join(__dirname, resolversPattern), (err, absolutes) => {
+//   if (err) console.error("Error while glob! ", err);
 
-  const filenames = absolutes.map(
-    (filename) => "." + filename.slice(__dirname.length)
-  );
+//   const filenames = absolutes.map(
+//     (filename) => "." + filename.slice(__dirname.length)
+//   );
 
-  let count = 0;
+//   let count = 0;
 
-  const output =
-    filenames
-      .map((filename) => {
-        return `var R${count++} = require("${filename}").default;`;
-      })
-      .join("\n") +
-    `\n\nmodule.exports = [${Array(count)
-      .fill(0)
-      .map((_, i) => "R" + i)}]`;
+//   const output =
+//     filenames
+//       .map((filename) => {
+//         return `var R${count++} = require("${filename}").default;`;
+//       })
+//       .join("\n") +
+//     `\n\nmodule.exports = [${Array(count)
+//       .fill(0)
+//       .map((_, i) => "R" + i)}]`;
 
-  writeFileSync("./dist/__resolvers.js", output);
-});
+//   writeFileSync("./dist/__resolvers.js", output);
+// });
