e6bedc1ee15c363dbfd06a620bc8c2af64e5b10c
BronzW <carlhugo.rydman@gmail.com>
2021-04-28 22:35:32 +0200

    Added favicon support

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -34,6 +34,12 @@ export default function App({
           name="keywords"
           content="developers,ideas,discuss,fun,programming,graphql,typescript,nextjs,firebase,tailwindcss,react,apollo"
         />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
     </ApolloProvider>
