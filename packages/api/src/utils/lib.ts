dac2bd7241ada1f5241e3a12bb26e9ab24371868
Bereket Semagn <semagnbereket@gmail.com>
2021-04-29 17:41:22 -0400

    refactor(api): insecure randomness

--- packages/api/src/utils/lib.ts
+++
@@ -1,5 +0,0 @@
-// Generate random number (# of digits is customizable)
-export const generatedNumber = (n = 10) => {
-  let multiplier = Math.pow(10, n - 1)
-  return Math.floor(1 * multiplier + Math.random() * 9 * multiplier);
-}
