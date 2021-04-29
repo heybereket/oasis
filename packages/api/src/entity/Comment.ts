ebdc181c6130c797aec66b65d88d2b376d0b24f0
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-29 18:33:01 +0100

    Use relations in Comment

--- packages/api/src/entity/Comment.ts
+++ packages/api/src/entity/Comment.ts
@@ -3,6 +3,7 @@ import { Field, Int, ObjectType, Root } from "type-graphql";
 import Post from "./Post";
 import { getRefData } from "../utils/getRefData";
 import User from "./User";
+import { Relation } from "../connection/Relation";
 
 @ObjectType()
 @Entity("comments")
@@ -19,16 +20,11 @@ export default class Comment extends BaseEntity {
   @Field(() => Int)
   dislikes: number;
 
-  _post: FirebaseFirestore.DocumentReference;
-  _author: FirebaseFirestore.DocumentReference;
-
   @Field(() => Post)
-  async post(@Root() parent: Comment) {
-    return getRefData(parent._post);
-  }
+  @Relation()
+  post: any;
 
   @Field(() => User)
-  async author(@Root() parent: Post) {
-    return getRefData(parent._author);
-  }
+  @Relation()
+  author: any;
 }
