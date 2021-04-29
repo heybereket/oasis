fc08a952e2a50c1c4f5353fbb662ef360e72d1e5
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-29 18:35:02 +0100

    We don't need "_" anymore!

--- packages/api/src/modules/auth/resolvers/Authenticate.ts
+++ packages/api/src/modules/auth/resolvers/Authenticate.ts
@@ -1,6 +1,6 @@
 import { adminDB } from "../../../utils/admin-db";
 import admin from "../../../utils/firebase-admin";
-import { generatedNumber } from "../../../utils/lib"
+import { generatedNumber } from "../../../utils/lib";
 import firebaseAdmin from "firebase-admin";
 import { Arg, Mutation, Resolver } from "type-graphql";
 import { ApolloError } from "apollo-server-errors";
@@ -21,7 +21,9 @@ export default class AuthenticateResolver {
       const docRef = adminDB.doc(`users/${decodedToken.uid}`);
       const doc = await docRef.get();
 
-      const checkUsername = adminDB.collection('users').where("username", "==", githubData.login)
+      const checkUsername = adminDB
+        .collection("users")
+        .where("username", "==", githubData.login);
       const usernameField = await checkUsername.get();
 
       // Get User Data and store in Firebase
@@ -33,21 +35,19 @@ export default class AuthenticateResolver {
         bio: null,
         twitter: null,
         url: null,
-        // To avoid variable naming conflicts in the entities,
-        // we use an "_" before any relational data fields
-        _activity: [],
-        _posts: [],
-        _comments: [],
-        _followers: [],
-        _following: []
+        activity: [],
+        posts: [],
+        comments: [],
+        followers: [],
+        following: [],
       };
 
       // Check if username is available
-      if (usernameField.empty && !doc.exists){
-        userData.username = `${githubData.login}`
+      if (usernameField.empty && !doc.exists) {
+        userData.username = `${githubData.login}`;
       } else if (!usernameField.empty && !doc.exists) {
         // Add generated digits to end of username if already exists in database
-        userData.username = `${githubData.login}${generatedNumber(6)}`
+        userData.username = `${githubData.login}${generatedNumber(6)}`;
       }
 
       // !doc.exists && !usernameField.empty
@@ -55,7 +55,7 @@ export default class AuthenticateResolver {
       // Add specific fields only if not already existed
       if (!doc.exists)
         userData.createdAt = firebaseAdmin.firestore.Timestamp.now();
-        userData.verified = false;
+      userData.verified = false;
 
       await docRef.set(userData, { merge: true });
 
