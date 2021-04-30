465d8a8624a2833e2300f06351bfce91e6951c7c
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-30 16:08:10 +0100

    create a base resolver

---
+++ packages/api/src/utils/createResolver.ts
@@ -0,0 +1,27 @@
+import { Arg, Query, Resolver } from "type-graphql";
+import { BaseEntity } from "../connection";
+
+export const createBaseResolver = (
+  suffix: string,
+  entity: typeof BaseEntity
+): any => {
+  @Resolver()
+  class BaseResolver {
+    @Query({ name: `all${suffix}s` })
+    all() {
+      return entity.find();
+    }
+
+    @Query({ name: `paginate${suffix}s` })
+    paginate(@Arg("limit") limit: number, @Arg("offset") offset: number) {
+      return entity.paginate(limit, offset);
+    }
+
+    @Query({ name: `get${suffix}` })
+    get(@Arg("id") id: string) {
+      return entity.findOne(id);
+    }
+  }
+
+  return BaseResolver;
+};
