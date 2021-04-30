73be41128c4cf6d29c2adf78a9be75ca0fb70356
Yoshida Tomio <mail@1chi.tk>
2021-04-30 08:39:12 +0800

    Update index.ts (#160)

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -1,7 +1,7 @@
 const { webkit } = require('playwright');
 
 async function run(){
-  const browser = await webkit.launch();
+  const browser = await webkit.launch({ headless: true, args: ['--disable-gpu'] });
   const page = await browser.newPage();
   await page.goto('http://oasis.sh');
   await browser.close();
