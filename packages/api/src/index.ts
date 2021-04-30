3c7ef1e6b7c2a0408ed6f22f352bd476b99611bc
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-04-30 21:27:29 +0100

    Get rid of annyoing logs

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,25 +1,25 @@
-import { join, dirname } from "path";
-import { config } from "dotenv";
+import { join, dirname } from 'path';
+import { config } from 'dotenv';
 
 const ROOT = process.env.PROJECT_ROOT
-  ? join(process.env.PROJECT_ROOT, "./packages/api")
+  ? join(process.env.PROJECT_ROOT, './packages/api')
   : dirname(__dirname);
 
-config({ path: ROOT + "/.env" });
+config({ path: ROOT + '/.env' });
 
-import "reflect-metadata";
+import 'reflect-metadata';
 
-import { GraphQLRequestContext } from "apollo-server-plugin-base";
-import { ApolloServer } from "apollo-server-micro";
-import admin from "./utils/firebase-admin";
-import { NextApiRequest } from "next";
+import { GraphQLRequestContext } from 'apollo-server-plugin-base';
+import { ApolloServer } from 'apollo-server-micro';
+import admin from './utils/firebase-admin';
+import { NextApiRequest } from 'next';
 
-import { getComplexity, simpleEstimator } from "graphql-query-complexity";
-import depthLimit from "graphql-depth-limit";
+import { getComplexity, simpleEstimator } from 'graphql-query-complexity';
+import depthLimit from 'graphql-depth-limit';
 
-import { getSchema } from "./utils/getSchema";
-import { TypeInfo, ValidationContext } from "graphql";
-import getDepth from "./utils/getDepth";
+import { getSchema } from './utils/getSchema';
+import { TypeInfo, ValidationContext } from 'graphql';
+import getDepth from './utils/getDepth';
 
 export { getSchema };
 
@@ -32,7 +32,7 @@ export const createApolloServer = async () => {
       const authHeader = req.headers.authorization;
       const socketInfo = req.socket.address();
 
-      if (!authHeader || !authHeader.startsWith("Bearer "))
+      if (!authHeader || !authHeader.startsWith('Bearer '))
         return { hasAuth: false, socketInfo };
 
       const token = authHeader.substring(7, authHeader.length);
@@ -63,8 +63,7 @@ export const createApolloServer = async () => {
                 query: context.document,
               });
 
-              getDepth(validationContext, (depths) => console.log(depths));
-              console.log(`Complexity: ${complexity}`);
+              getDepth(validationContext);
             },
           };
         },
