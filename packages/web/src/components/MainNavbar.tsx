79cd9e6b7cae3574aa01f962f813c6145bb69e51
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-01 18:09:52 +0100

    fix(web): responsive and epic navbar

--- packages/web/src/components/MainNavbar.tsx
+++ packages/web/src/components/MainNavbar.tsx
@@ -15,10 +15,10 @@ export const Navbar: React.FC = () => {
       <nav className="hidden sm:flex items-center justify-between p-8 bg-gray-800 ">
         <div className="flex justify-items-start items-center space-x-5">
           <img src="/static/oasis-logo.png" alt="Oasis Logo" className="w-28" />
-          <NavItem name="Home" href="#" />
-          <NavItem name="Topics" href="#" />
-          <NavItem name="Friends" href="#" />
-          <NavItem name="Saved" href="#" />
+          <NavItem name="Home" href="#" mobile={false} />
+          <NavItem name="Topics" href="#" mobile={false} />
+          <NavItem name="Friends" href="#" mobile={false} />
+          <NavItem name="Saved" href="#" mobile={false} />
         </div>
         <div className="flex justify-items-start items-center space-x-6 ml-4">
           <div className="flex items-center relative">
@@ -36,7 +36,11 @@ export const Navbar: React.FC = () => {
               className="w-12 h-12 rounded-full"
             />
           )}
-          <img src="/static/Down-Arrow.svg" />
+          {user ? (
+            <img src="/static/Down-Arrow.svg" />
+          ) : (
+            <Button size="sm">Login</Button>
+          )}
         </div>
       </nav>
 
@@ -44,85 +48,45 @@ export const Navbar: React.FC = () => {
       <nav className="max-w-full mx-auto p-8 sm:hidden">
         <div className="flex items-center justify-between">
           <img src="/static/oasis-logo.png" alt="Oasis Logo" className="w-28" />
-          <ul className="flex items-center space-x-6 md:space-x-10 text-lg text-gray-300">
-            <li>
-              <img src="/static/mobile-magnifying-glass.svg" />
-            </li>
-            <li>
-              <svg
-                width="26"
-                height="28"
-                viewBox="0 0 26 28"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15.0183 24.791C14.805 25.1588 14.4988 25.464 14.1304 25.6762C13.7621 25.8883 13.3444 26 12.9193 26C12.4942 26 12.0766 25.8883 11.7082 25.6762C11.3399 25.464 11.0337 25.1588 10.8204 24.791M20.1989 9.01869C20.1989 7.08803 19.4319 5.23645 18.0667 3.87127C16.7016 2.50609 14.85 1.73914 12.9193 1.73914C10.9887 1.73914 9.13709 2.50609 7.77191 3.87127C6.40673 5.23645 5.63978 7.08803 5.63978 9.01869C5.63978 17.5115 2 19.938 2 19.938H23.8387C23.8387 19.938 20.1989 17.5115 20.1989 9.01869Z"
-                  stroke="white"
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </li>
-            <li>
-              {user && (
-                <img
-                  onClick={() => {
-                    toggleIsOn(!isOn);
-                  }}
-                  src={user?.photoURL ?? undefined}
-                  alt={user?.displayName ?? undefined}
-                  className="w-9 h-9 rounded-full mr-8"
-                />
-              )}
-            </li>
-          </ul>
-          {/* <Button
-            onClick={() => {
-              toggleIsOn(!isOn);
-            }}
-            className="w-10 h-10 flex items-center justify-center text-xl"
-            color="gray"
-          >
-            ☰
-          </Button> */}
+          <div className="flex items-center space-x-6 md:space-x-10 text-lg text-gray-300">
+            <img src="/static/Search.svg" />
+            <img src="/static/Bell.svg" />
+
+            {user ? (
+              <img
+                onClick={() => {
+                  toggleIsOn(!isOn);
+                }}
+                src={user?.photoURL ?? undefined}
+                alt={user?.displayName ?? undefined}
+                className="w-12 h-12 rounded-full"
+              />
+            ) : (
+              <Button size="sm">Login</Button>
+            )}
+          </div>
         </div>
 
         <div
-          className={
-            isOn
-              ? 'max-w-md flex rounded-lg justify-end animate-fade-in-down bg-gray-700 float-right px-6 py-2'
-              : 'opacity-0 flex rounded-lg animate-fade-out-up bg-gray-700 max-w-md float-right px-6 py-2 justify-end'
-          }
+          className={`flex rounded-lg justify-end bg-gray-700 float-right px-3 py-2 mt-2 max-w-md ${
+            isOn ? 'animate-fade-in-down' : 'opacity-0 animate-fade-out-up'
+          }`}
         >
-          <ul className="flex items-center space-x-6 md:space-x-10 text-base text-gray-300">
-            <li>
-              <a href="#" className="hover:text-gray-200">
-                Feed
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-200">
-                Followers
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  firebase.auth().signOut();
-                  router.push('/');
-                }}
-                href="#"
-                className="hover:text-gray-200"
-              >
-                Logout
-              </a>
-            </li>
-            <li>
-              <Button size="sm">Post</Button>
-            </li>
-          </ul>
+          <div className="flex items-center space-x-6 md:space-x-10 text-base text-gray-300">
+            <NavItem name="Feed" href="#" mobile={true} />
+
+            <NavItem name="Followers" href="#" mobile={true} />
+            <NavItem
+              name="Logout"
+              onClick={() => {
+                firebase.auth().signOut();
+                router.push('/');
+              }}
+              mobile={true}
+            />
+
+            <Button size="sm">Post</Button>
+          </div>
         </div>
       </nav>
     </>
--- packages/web/src/components/NavItem.tsx
+++ packages/web/src/components/NavItem.tsx
@@ -1,10 +1,14 @@
+import { MouseEventHandler } from 'react';
+
 interface NavItemProps {
-  href: string;
   name: string;
+  mobile: boolean;
+  href?: string;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
 }
 
 export const NavItem: React.FC<NavItemProps> = (props: NavItemProps) => {
-  return (
+  return !props.mobile ? (
     <div className="flex justify-between">
       <img className="mr-3" src={`/static/${props.name}.svg`} />
       <a
@@ -14,5 +18,13 @@ export const NavItem: React.FC<NavItemProps> = (props: NavItemProps) => {
         {props.name}
       </a>
     </div>
+  ) : (
+    <a
+      href={props.href ? props.href : '#'}
+      onClick={props.onClick}
+      className="hover:text-gray-200"
+    >
+      {props.name}
+    </a>
   );
 };
