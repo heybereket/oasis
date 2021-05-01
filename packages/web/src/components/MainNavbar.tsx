bac6a2c73f2256b5e9802df836e7233dddc53398
Henry LeCompte <32208783+HenryLeC@users.noreply.github.com>
2021-05-01 09:28:31 -0400

    Navbar design (#170)
    * feat(web): Added Desktop navbar design
    
    * feat(web): Added mobile navbar

--- packages/web/src/components/MainNavbar.tsx
+++ packages/web/src/components/MainNavbar.tsx
@@ -11,33 +11,67 @@ export const Navbar: React.FC = () => {
 
   return (
     <>
-      <nav className="hidden max-w-5xl mx-auto sm:flex items-center justify-between py-8">
-        <img src="/static/oasis-logo.png" alt="Oasis Logo" className="w-32" />
+      <nav className="hidden max-w-full mx-auto sm:flex items-center justify-between py-8 bg-gray-800">
+        <img
+          src="/static/oasis-logo.png"
+          alt="Oasis Logo"
+          className="w-32 ml-8"
+        />
         <ul className="flex items-center space-x-6 md:space-x-10 text-lg text-gray-300">
-          <li>
-            <a href="#" className="hover:text-gray-200">
-              Feed
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-gray-200">
-              Followers
-            </a>
-          </li>
-          <li>
+          <li className="flex justify-between">
+            <img src="/static/Home.svg" />
+            <div className="w-3.5" />
             <a
-              onClick={() => {
-                firebase.auth().signOut();
-                router.push('/');
-              }}
               href="#"
-              className="hover:text-gray-200"
+              className="font-extrabold text-white hover:text-gray-200"
             >
-              Logout
+              Home
             </a>
           </li>
+          <li className="flex justify-between">
+            <img src="/static/Topics.svg" />
+            <div className="w-3.5" />
+            <a
+              href="#"
+              className="font-extrabold text-white hover:text-gray-200"
+            >
+              Topics
+            </a>
+          </li>
+          <li className="flex justify-between">
+            <img src="/static/Friends.svg" />
+            <div className="w-3.5" />
+            <a
+              href="#"
+              className="font-extrabold text-white hover:text-gray-200"
+            >
+              Friends
+            </a>
+          </li>
+          <li className="flex justify-between">
+            <img src="/static/Saved.svg" />
+            <div className="w-3.5" />
+            <a
+              href="#"
+              className="font-extrabold text-white hover:text-gray-200"
+            >
+              Saved
+            </a>
+          </li>
+        </ul>
+        <ul className="flex items-center space-x-6 md:space-x-10 text-lg text-gray-300">
+          <li className="flex justify-left">
+            <img
+              src="/static/magnifying-glass.svg"
+              style={{ marginRight: '-25px', zIndex: 20 }}
+            />
+            <input
+              placeholder="Search for People, Posts, etc..."
+              className="rounded-lg w-80 bg-gray-700 h-10 text-sm pl-11"
+            />
+          </li>
           <li>
-            <Button size="sm">Post</Button>
+            <img src="/static/Bell.svg" />
           </li>
           <li>
             {user && (
@@ -48,14 +82,55 @@ export const Navbar: React.FC = () => {
               />
             )}
           </li>
+          <li>
+            <img src="/static/Down-Arrow.svg" className="mr-8" />
+          </li>
         </ul>
       </nav>
 
       {/*mobile navbar*/}
-      <nav className="sm:hidden max-w-5xl mx-auto py-8">
+      <nav className="sm:hidden max-w-full mx-auto py-8">
         <div className="flex items-center justify-between">
-          <img src="/static/oasis-logo.png" alt="Oasis Logo" className="w-32" />
-          <Button
+          <img
+            src="/static/oasis-logo.png"
+            alt="Oasis Logo"
+            className="w-32 ml-8"
+          />
+          <ul className="flex items-center space-x-6 md:space-x-10 text-lg text-gray-300">
+            <li>
+              <img src="/static/mobile-magnifying-glass.svg" />
+            </li>
+            <li>
+              <svg
+                width="26"
+                height="28"
+                viewBox="0 0 26 28"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15.0183 24.791C14.805 25.1588 14.4988 25.464 14.1304 25.6762C13.7621 25.8883 13.3444 26 12.9193 26C12.4942 26 12.0766 25.8883 11.7082 25.6762C11.3399 25.464 11.0337 25.1588 10.8204 24.791M20.1989 9.01869C20.1989 7.08803 19.4319 5.23645 18.0667 3.87127C16.7016 2.50609 14.85 1.73914 12.9193 1.73914C10.9887 1.73914 9.13709 2.50609 7.77191 3.87127C6.40673 5.23645 5.63978 7.08803 5.63978 9.01869C5.63978 17.5115 2 19.938 2 19.938H23.8387C23.8387 19.938 20.1989 17.5115 20.1989 9.01869Z"
+                  stroke="white"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </li>
+            <li>
+              {user && (
+                <img
+                  onClick={() => {
+                    toggleIsOn(!isOn);
+                  }}
+                  src={user?.photoURL ?? undefined}
+                  alt={user?.displayName ?? undefined}
+                  className="w-9 h-9 rounded-full mr-8"
+                />
+              )}
+            </li>
+          </ul>
+          {/* <Button
             onClick={() => {
               toggleIsOn(!isOn);
             }}
@@ -63,7 +138,7 @@ export const Navbar: React.FC = () => {
             color="gray"
           >
             ☰
-          </Button>
+          </Button> */}
         </div>
 
         <div
@@ -98,15 +173,6 @@ export const Navbar: React.FC = () => {
             </li>
             <li>
               <Button size="sm">Post</Button>
-            </li>
-            <li className={user ? '' : 'hidden'}>
-              {user && (
-                <img
-                  src={user?.photoURL ?? undefined}
-                  alt={user?.displayName ?? undefined}
-                  className="w-12 h-12 rounded-full"
-                />
-              )}
             </li>
           </ul>
         </div>
