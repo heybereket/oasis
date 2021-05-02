f5905ff053f32b9a753449446dda4ca154967517
Henry LeCompte <henry@volunteerio.us>
2021-05-02 08:53:58 -0400

    refactor(api): Removed redundant profile mutations

--- packages/api/src/modules/user/resolvers/UpdateProfile.ts
+++ packages/api/src/modules/user/resolvers/UpdateProfile.ts
@@ -9,19 +9,6 @@ import UpdateProfileInput from '../UpdateProfileInput';
 @Resolver()
 export default class UpdateProfile {
   @Mutation(() => Boolean)
-  async changeBanner(@Arg('bannerUrl') bannerUrl: string, @Ctx() ctx: any) {
-    if (ctx.hasAuth) {
-      adminDB
-        .collection('users')
-        .doc(ctx.uid)
-        .set({ banner: bannerUrl }, { merge: true });
-      return true;
-    } else {
-      return new AuthenticationError('Unable to edit profile. Try logging in.');
-    }
-  }
-
-  @Mutation(() => Boolean)
   @Authorized()
   async updateProfile(@Arg('data') data: UpdateProfileInput, @Ctx() ctx: any) {
     if (data.username)
--- packages/api/src/modules/user/resolvers/User.ts
+++ packages/api/src/modules/user/resolvers/User.ts
@@ -1,9 +1,6 @@
 import User from '../../../entity/User';
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Query, Resolver } from 'type-graphql';
 import { ContextType } from '../../../utils/contextFromToken';
-import { AuthenticationError } from 'apollo-server-errors';
-import { adminDB } from '../../../utils/common/admin-db';
-import { generateSafeUsername } from '../../../utils/generateSafeUsername';
 
 @Resolver()
 export default class UserResolver {
@@ -14,78 +11,5 @@ export default class UserResolver {
   ) {
     const users = await User.query('username', username.toLowerCase());
     return users;
-  }
-
-  @Mutation(() => Boolean)
-  async changeBanner(@Arg('bannerUrl') bannerUrl: string, @Ctx() ctx: any) {
-    if (ctx.hasAuth) {
-      adminDB
-        .collection('users')
-        .doc(ctx.uid)
-        .set({ banner: bannerUrl }, { merge: true });
-      return true;
-    } else {
-      return new AuthenticationError('Please login to change your banner.');
-    }
-  }
-
-  @Mutation(() => Boolean)
-  async changeBio(@Arg('bio') newBio: string, @Ctx() ctx: any) {
-    if (ctx.hasAuth) {
-      adminDB
-        .collection('users')
-        .doc(ctx.uid)
-        .set({ bio: newBio }, { merge: true });
-      return true;
-    } else {
-      return new AuthenticationError('Please log in to change your bio');
-    }
-  }
-
-  @Mutation(() => Boolean)
-  async changeUsername(@Arg('username') newUsername: string, @Ctx() ctx: any) {
-    if (ctx.hasAuth) {
-      adminDB
-        .collection('users')
-        .doc(ctx.uid)
-        .set(
-          { username: await generateSafeUsername(newUsername.toLowerCase()) },
-          { merge: true }
-        );
-      return true;
-    } else {
-      return new AuthenticationError('Please log in to change your username');
-    }
-  }
-
-  @Mutation(() => Boolean)
-  async changeDisplayName(
-    @Arg('displayName') newName: string,
-    @Ctx() ctx: any
-  ) {
-    if (ctx.hasAuth) {
-      adminDB
-        .collection('users')
-        .doc(ctx.uid)
-        .set({ name: newName }, { merge: true });
-      return true;
-    } else {
-      return new AuthenticationError(
-        'Please log in to change your display name'
-      );
-    }
-  }
-
-  @Mutation(() => Boolean)
-  async changeAvatar(@Arg('avatar') newAvatar: string, @Ctx() ctx: any) {
-    if (ctx.hasAuth) {
-      adminDB
-        .collection('users')
-        .doc(ctx.uid)
-        .set({ avatar: newAvatar }, { merge: true });
-      return true;
-    } else {
-      return new AuthenticationError('Please log in to change your avatar');
-    }
   }
 }
