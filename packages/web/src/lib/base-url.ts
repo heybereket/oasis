cc657f8c4355e106736ec8c7c4a43ca863eeb586
Henry LeCompte <henry@volunteerio.us>
2021-05-01 20:12:18 -0400

    fix(web): SECURE_MODE needs to be prefaced with NEXT_PUBLIC_

--- packages/web/src/lib/base-url.ts
+++ packages/web/src/lib/base-url.ts
@@ -1,7 +1,9 @@
 export default function getAPIBaseURL(): string {
   let baseUrl = '';
 
-  process.env.SECURE_MODE ? (baseUrl += `https://`) : (baseUrl += `http://`);
+  process.env.NEXT_PUBLIC_SECURE_MODE == 'true'
+    ? (baseUrl += `https://`)
+    : (baseUrl += `http://`);
   baseUrl += process.env.NEXT_PUBLIC_BASE_API_URL;
   baseUrl += '/api/graphql';
 
