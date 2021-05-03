9df7114d1c6253d5f6e4ec1cc283ec9da11cd7fb
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-03 19:22:33 +0100

    refactor(api): UpdateProfile resolver can work

--- packages/api/src/modules/user/UpdateProfile.resolver.ts
+++ packages/api/src/modules/user/UpdateProfile.resolver.ts
@@ -3,21 +3,22 @@ import { Arg, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
 import { AuthenticationError } from "apollo-server-errors";
 import { generateSafeUsername } from "../../utils/generateSafeUsername";
 import UpdateProfileInput from "./UpdateProfileInput";
+import { ContextType } from "../../apolloServer";
 
 @Resolver()
 export default class UserResolver {
   @Mutation(() => Boolean)
   @Authorized()
-  async updateProfile(@Arg("data") data: UpdateProfileInput, @Ctx() ctx: any) {
-    if (!ctx.hasAuth)
-      return new AuthenticationError(
-        "You must be logged in to change your profile."
-      );
+  async updateProfile(
+    @Arg("data") data: UpdateProfileInput,
+    @Ctx() { getUser }: ContextType
+  ) {
     if (data.username)
       data.username = await generateSafeUsername(data.username.toLowerCase());
 
-    const user = await User.findOne(ctx.uid);
+    const user = await getUser();
     User.merge(user, data);
+
     await user.save();
     return true;
   }
