16d061f7dbbc775e5b7bf5f1e876de4d3d323080
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-03 20:39:16 +0100

    feat(api): Add support for different dbs

--- packages/api/src/ormconfig.ts
+++ packages/api/src/ormconfig.ts
@@ -4,7 +4,7 @@ import { join } from "path";
 export const isProd = process.env.NODE_ENV === "production";
 
 export const ormconfig: ConnectionOptions = {
-  type: "postgres",
+  type: (process.env.DATABASE_TYPE as any) || "postgres",
   url: process.env.DATABASE_URL,
   synchronize: !isProd,
   entities: [join(__dirname, "./entities/*.*")],
