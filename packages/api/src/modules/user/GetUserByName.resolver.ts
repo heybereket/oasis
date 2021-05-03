41bcdbc8065406485585620337cf688abcc991c0
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-03 06:14:36 +0100

    Use the BaseEntity

--- packages/api/src/modules/user/GetUserByName.resolver.ts
+++ packages/api/src/modules/user/GetUserByName.resolver.ts
@@ -1,13 +1,10 @@
 import { Query, Arg } from "type-graphql";
-import { getRepository } from "typeorm";
 import User from "../../entities/User";
 
 export class GetUserByNameResolver {
   @Query(() => User, { nullable: true })
   async getUserByName(@Arg("username") username: string) {
-    const repo = getRepository(User);
-    const user: User = await repo
-      .createQueryBuilder()
+    const user: User = await User.createQueryBuilder()
       .where("LOWER(username) = :username", {
         username: username.toLowerCase(),
       })
