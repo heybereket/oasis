d10d7d9807d404c1ab78e5c72364b2ca79933601
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-03 07:03:47 +0100

    refactor(api): Context from token file

--- packages/api/src/utils/contextFromToken.ts
+++ packages/api/src/utils/contextFromToken.ts
@@ -3,21 +3,29 @@ import { IncomingMessage } from "http";
 
 type SocketInfo = ReturnType<IncomingMessage["socket"]["address"]>;
 
+export type AuthedContextType = {
+  uid: string;
+  hasAuth: true;
+  socketInfo: SocketInfo;
+};
+
+export type NotAuthedContextType = {
+  hasAuth: false;
+  socketInfo: SocketInfo;
+};
+
+export type ContextType = AuthedContextType | NotAuthedContextType;
+
 export const contextFromToken = async (
   token: string,
   socketInfo: SocketInfo
-) => {
+): Promise<ContextType> => {
   try {
     throw new Error("auth not yet implemented");
-    const data = {};
+    const data = { uid: "hello" };
     // const data = await admin.auth().verifyIdToken(token);
     return { hasAuth: true, ...data, socketInfo };
   } catch (e) {
     return { hasAuth: false, socketInfo };
   }
 };
-
-// note PromiseLike instead of Promise, this lets it work on any thenable
-type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
-
-export type ContextType = ThenArg<ReturnType<typeof contextFromToken>>;
