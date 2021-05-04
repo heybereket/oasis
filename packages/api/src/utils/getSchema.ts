81c27be21b5f5ea81c67d291569a89df14bb3b6b
Henry LeCompte <henry@volunteerio.us>
2021-05-04 17:49:36 -0400

    fix(build): Fixed build scripts

--- packages/api/src/utils/getSchema.ts
+++ packages/api/src/utils/getSchema.ts
@@ -3,7 +3,9 @@ import { join } from "path";
 
 export const getSchema = () => {
   return buildSchema({
-    resolvers: [join(process.env.NEXT_SRC_PATH, "./modules/**/*.resolver.js")],
+    resolvers: [
+      join(process.env.NEXT_SRC_PATH, "../dist/modules/**/*.resolver.js"),
+    ],
     emitSchemaFile:
       process.env.NODE_ENV === "development"
         ? join(process.env.NEXT_SRC_PATH, "../schema.gql")
