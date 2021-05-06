c578eea8ec9af08d9689d987d2a00f4f81a3b504
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-06 20:32:53 +0100

    refactor(web): refactor About icon

---
+++ packages/web/src/components/icons/About.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const BellIcon: React.FC<React.SVGProps<SVGElement>> = (
+  { children: _ },
+  props
+) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ffffff"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+      <circle cx="12" cy="7" r="4"></circle>
+    </svg>
+  );
+};
