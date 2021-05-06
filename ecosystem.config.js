0af1b9daa7e2fc742e78d6c52bc4d7b523d7b22e
Sam Jakob Mearns <me@samjakob.com>
2021-05-06 20:48:41 +0100

    Update PM2 ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,6 +19,7 @@ module.exports = {
       ref: 'origin/staging',
       repo: 'https://github.com/oasis-sh/oasis.git',
       path: '/opt/oasis/staging',
+      ssh_options: "StrictHostKeyChecking=no",
       'pre-deploy-local': '',
       'post-deploy' : 'yarn && yarn build && env PM2_HOME=/opt/oasis/.pm2 pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
@@ -30,6 +31,7 @@ module.exports = {
       ref: 'origin/staging', // For now, production will track staging branches
       repo: 'https://github.com/oasis-sh/oasis.git',
       path: '/opt/oasis/production',
+      ssh_options: "StrictHostKeyChecking=no",
       'pre-deploy-local': '',
       'post-deploy' : 'yarn && yarn build && env PM2_HOME=/opt/oasis/.pm2 pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
