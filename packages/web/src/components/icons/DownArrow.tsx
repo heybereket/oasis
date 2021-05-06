8338ed0046eaa1302c0be66fd63386fb22ea28ca
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-06 20:40:40 +0100

    refactor(web): multiple icons

--- packages/web/src/components/icons/DownArrow.tsx
+++ packages/web/src/components/icons/DownArrow.tsx
@@ -1,3 +1,25 @@
-<svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1 1L7 7L13 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+import React from 'react';
+
+export const Twitter: React.FC<React.SVGProps<SVGElement>> = (
+  { children: _ },
+  props
+) => {
+  return (
+    <svg
+      width="14"
+      height="8"
+      viewBox="0 0 14 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M1 1L7 7L13 1"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
