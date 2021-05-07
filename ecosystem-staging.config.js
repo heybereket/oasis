c8dd89512454d216b4cb8f3d02a50c7cfbec906a
Henry LeCompte <henry@volunteerio.us>
2021-05-06 21:03:44 -0400

    fix(pm2): Added different configs foor different deployments

--- ecosystem-staging.config.js
+++ ecosystem-staging.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'oasis',
+      name: 'oasis-staging',
       script: 'yarn',
       args: 'start',
       interpreter: 'none',
       env: {
         NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
       },
     },
   ],
@@ -21,19 +18,6 @@ module.exports = {
       ref: 'origin/staging',
       repo: 'https://github.com/oasis-sh/oasis.git',
       path: '/opt/oasis/staging',
-      ssh_options: 'StrictHostKeyChecking=no',
-      'pre-deploy-local': '',
-      'post-deploy':
-        'yarn && yarn build && yarn workspace @oasis/api typeorm:run_migrations && env PM2_HOME=/opt/oasis/.pm2 pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-    },
-
-    production: {
-      user: 'ci-runner',
-      host: 'oasis-deploy',
-      ref: 'origin/prod',
-      repo: 'https://github.com/oasis-sh/oasis.git',
-      path: '/opt/oasis/production',
       ssh_options: 'StrictHostKeyChecking=no',
       'pre-deploy-local': '',
       'post-deploy':
