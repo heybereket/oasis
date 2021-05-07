a1d60db8d02b5b79e7eb300a822c4046d154122f
Bereket Semagn <semagnbereket@gmail.com>
2021-05-06 21:46:43 -0400

    refactor(web): simplify getAPIBaseURL() & lint

--- packages/web/src/lib/base-url.ts
+++ packages/web/src/lib/base-url.ts
@@ -1,5 +1,5 @@
 export function getAPIBaseURL(): string {
-  return `http${process.env.NEXT_PUBLIC_SECURE_MODE === "true" ? 's' : ''}://${process.env.NEXT_PUBLIC_BASE_API_URL}`;
+  return `${process.env.NEXT_PUBLIC_BASE_API_URL}`;
 }
 
 export function getGQLBaseURL(): string {
