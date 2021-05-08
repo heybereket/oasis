27a277bec90b6ab8ddc3c8e9d28ddf1d2abd14f4
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-08 18:08:09 +0100

    refactor(api): Clean up the emit schema [no ci]

--- packages/api/src/utils/getSchema.ts
+++ packages/api/src/utils/getSchema.ts
@@ -1,5 +1,4 @@
 import { buildSchema } from 'type-graphql';
-import { join } from 'path';
 import { joinRoot } from './rootPath';
 import { customAuthChecker } from './auth/authChecker';
 
@@ -7,9 +6,7 @@ export const getSchema = () => {
   return buildSchema({
     resolvers: [joinRoot('./modules/**/*.resolver.js')],
     emitSchemaFile:
-      process.env.NODE_ENV === 'development'
-        ? join(process.env.OASIS_API_SRC_PATH ?? '', '../schema.gql')
-        : false,
+      process.env.NODE_ENV === 'development' && joinRoot('../schema.gql'),
     authChecker: customAuthChecker,
   });
 };
