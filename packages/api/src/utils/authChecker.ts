0542eb17a12675db00d47764df208d1ce4a6a835
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-08 07:15:30 +0100

    refactor(authChecker): Refactor auth checker code

--- packages/api/src/utils/authChecker.ts
+++ packages/api/src/utils/authChecker.ts
@@ -5,20 +5,15 @@ export const customAuthChecker: AuthChecker<ContextType> = async (
   { context },
   roles
 ) => {
-  console.log(context.hasAuth);
   if (context.hasAuth) {
     const user = await context.getUser();
 
-    let authorized = true;
+    for (const role of roles) {
+      if (!user.roles.includes(role)) return false;
+    }
 
-    roles.forEach((role) => {
-      if (!user.roles.includes(role)) {
-        authorized = false;
-      }
-    });
-
-    return authorized;
-  } else {
-    return false;
+    return true;
   }
+
+  return false;
 };
