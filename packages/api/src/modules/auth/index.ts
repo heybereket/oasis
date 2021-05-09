347c4e71be2d1daa1175e6cf497666519a924997
Bereket Semagn <semagnbereket@gmail.com>
2021-05-09 08:03:07 -0400

    Revert "Twitter oauth (#184)"
    This reverts commit 81a7f01c1644a4dd2cb3bb447891bdaac5e4630a.

--- packages/api/src/modules/auth/index.ts
+++ packages/api/src/modules/auth/index.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 import { PassportStatic } from 'passport';
 import GitHubAuth from './providers/github';
-import TwitterAuth from './providers/twitter';
 
 export default (passport: PassportStatic): Router => {
   const authRouter = Router();
 
   /** Third party auth services */
   authRouter.use('/github', GitHubAuth(passport));
-  authRouter.use('/twitter', TwitterAuth(passport));
 
   /** Internal actions. */
   authRouter.get('/logout', (req, res) => {
