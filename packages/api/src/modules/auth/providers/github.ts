e1aa4f0d7d57fb985d03262b60dec217913d28c3
Bereket Semagn <semagnbereket@gmail.com>
2021-05-11 17:55:41 -0400

    feat(api): add joined column for auth

--- packages/api/src/modules/auth/providers/github.ts
+++ packages/api/src/modules/auth/providers/github.ts
@@ -3,6 +3,7 @@ import { Strategy } from 'passport-github2';
 import User from '@entities/User';
 import { v4 as uuid } from 'uuid';
 import { checkUsername } from '@utils/auth/checkUsername';
+import { getShortMonth } from '@lib';
 import { PassportStatic } from 'passport';
 
 export default (passport: PassportStatic): Router => {
@@ -29,6 +30,7 @@ export default (passport: PassportStatic): Router => {
             user.github = id;
             user.verified = false;
             user.createdAt = String(Date.now());
+            user.joined = getShortMonth()
           }
 
           await user.save();
