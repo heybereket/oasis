2e6234c923f2a060e2affd118f68b9e4b64242aa
Bereket Semagn <semagnbereket@gmail.com>
2021-05-12 18:14:20 -0400

    fix(web): verified badge for verified users

--- packages/web/src/pages/user/[username]/index.tsx
+++ packages/web/src/pages/user/[username]/index.tsx
@@ -92,7 +92,7 @@ const Profile: React.FC<ProfileProps> = (props) => {
                   )}
 
               <div className="flex">
-                {(data?.contributor === true) && (
+                {(data?.verified === true) && (
                     <span className="tracking-wider text-white bg-[#306EEA] px-4 py-1 mt-3 mr-2 text-sm rounded-full h-8 w-8 flex items-center justify-center leading-loose font-semibold" title="">
                       V
                     </span>
