bee863309f675d5917b73692b124e27ad8339df9
Bereket Semagn <semagnbereket@gmail.com>
2021-05-12 19:33:18 -0400

    feat(web): verified and contributor badge icons

---
+++ packages/web/src/components/icons/badges/Code.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Code: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...props}
+    >
+      <polyline points="16 18 22 12 16 6" />
+      <polyline points="8 6 2 12 8 18" />
+  </svg>
+  );
+};
