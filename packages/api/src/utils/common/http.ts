6dde683d50109c2aadf9a4b1ce07e40e95a5bcc2
Bereket Semagn <semagnbereket@gmail.com>
2021-05-12 16:07:15 -0400

    chore(api): add space to http util file

--- packages/api/src/utils/common/http.ts
+++ packages/api/src/utils/common/http.ts
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+
 export const http = async (request: string): Promise<any> => {
   const response = await fetch(request);
   const body = await response.json();
