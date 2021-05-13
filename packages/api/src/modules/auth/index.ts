b818a7bf6f817272c99497ec245a639c75adaf5b
Henry LeCompte <henry@volunteerio.us>
2021-05-12 21:16:50 -0400

    fix(api): OAuth creds are now optional
    Github creds are still needed by CheckAuth

--- packages/api/src/modules/auth/index.ts
+++ packages/api/src/modules/auth/index.ts
@@ -8,9 +8,12 @@ export default (passport: PassportStatic): Router => {
   const authRouter = Router();
 
   /** Third party auth services */
-  authRouter.use('/github', GitHubAuth(passport));
-  authRouter.use('/twitter', TwitterAuth(passport));
-  authRouter.use('/discord', DiscordAuth(passport));
+  if (process.env.OASIS_API_GITHUB_CLIENT_ID)
+    authRouter.use('/github', GitHubAuth(passport));
+  if (process.env.OASIS_API_TWITTER_KEY)
+    authRouter.use('/twitter', TwitterAuth(passport));
+  if (process.env.OASIS_API_DISCORD_CLIENT_ID)
+    authRouter.use('/discord', DiscordAuth(passport));
   /** Internal actions. */
   authRouter.get('/logout', (req, res) => {
     req.logout();
