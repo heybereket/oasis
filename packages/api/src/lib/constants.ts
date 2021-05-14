725db0a426b605c1226f590f7602ed7f4806c615
Bereket Semagn <semagnbereket@gmail.com>
2021-05-13 21:10:32 -0400

    refactor(api): add isProduction and isDevelopment consts

---
+++ packages/api/src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const isProduction = process.env.NODE_ENV === 'production';
+export const isDevelopment = process.env.NODE_ENV === 'development';
--- packages/api/src/utils/getSchema.ts
+++ packages/api/src/utils/getSchema.ts
@@ -1,12 +1,12 @@
 import { buildSchema } from 'type-graphql';
 import { joinRoot } from './common/rootPath';
 import { customAuthChecker } from '@utils/auth/authChecker';
+import { isProduction } from '@lib/constants'
 
 export const getSchema = () => {
   return buildSchema({
     resolvers: [joinRoot('./modules/**/*.resolver.js')],
-    emitSchemaFile:
-      process.env.NODE_ENV === 'development' && joinRoot('../schema.gql'),
+    emitSchemaFile: isProduction && joinRoot('../schema.gql'),
     authChecker: customAuthChecker,
   });
 };
