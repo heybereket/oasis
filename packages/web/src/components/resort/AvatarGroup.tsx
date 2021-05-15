8b019a0e2ea3c3675b4fd3b06b9205466cf8ba1e
Henry LeCompte <henry@volunteerio.us>
2021-05-15 12:24:54 -0400

    feat(web): Added data loading to resort

--- packages/web/src/components/resort/AvatarGroup.tsx
+++ packages/web/src/components/resort/AvatarGroup.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const AvatarGroup: React.FC = () => {
+interface IAvatarGroupProps {
+  avatarIcons: string[];
+}
+
+const AvatarGroup: React.FC<IAvatarGroupProps> = ({ avatarIcons }) => {
   return (
     <div className="flex -space-x-1 overflow-hidden">
-      <img
-        className="inline-block h-6 w-6 rounded-full"
-        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
-      <img
+      {avatarIcons.map((icon) => (
+        <img
+          key={icon}
+          className="inline-block h-6 w-6 rounded-full"
+          src={icon}
+          alt=""
+        />
+      ))}
+      {/* <img
         className="inline-block h-6 w-6 rounded-full"
         src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         alt=""
@@ -22,7 +29,7 @@ const AvatarGroup: React.FC = () => {
         className="inline-block h-6 w-6 rounded-full"
         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         alt=""
-      />
+      /> */}
     </div>
   );
 };
