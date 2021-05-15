af2fa721f270a149bd46633dfa6c6bf2dd03009d
BronzW <50546858+BronzW@users.noreply.github.com>
2021-05-15 23:00:09 +0200

    feat(api): Added Notifications (#233)
    * feat(api): notifications
    
    * fix(api): fixes
    
    * fix(api): more fixes

---
+++ packages/api/src/entities/Notification.ts
@@ -0,0 +1,38 @@
+import { NotificationType } from '@modules/notification/Notifications';
+import { Field, ID, ObjectType } from 'type-graphql';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import User from './User';
+
+@ObjectType()
+@Entity()
+export default class Notification extends BaseEntity {
+  @Field()
+  @Column({ type: 'date' })
+  createdAt: string;
+
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => NotificationType)
+  @Column()
+  type: NotificationType;
+
+  @Field(() => User)
+  @ManyToOne(() => User)
+  user: Promise<User>;
+
+  @Field(() => User, { nullable: true })
+  @ManyToOne(() => User, { nullable: true })
+  performer: Promise<User>;
+
+  @Field()
+  @Column()
+  read: boolean = false;
+}
