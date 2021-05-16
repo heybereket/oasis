e8e1f824a4953c76eb57e6bd1bd45932b38dab6d
Bereket Semagn <semagnbereket@gmail.com>
2021-05-16 19:30:59 -0400

    fix(web): profile icon invalid dom properties

--- packages/web/src/components/icons/Profile.tsx
+++ packages/web/src/components/icons/Profile.tsx
@@ -9,9 +9,9 @@ export const Profile: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
     viewBox="0 0 24 24"
     fill="none"
     stroke="#ffffff"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
