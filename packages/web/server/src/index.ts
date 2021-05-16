4330bcea6c8d5f6d620704eb086ce5caa2c49aae
Bereket Semagn <semagnbereket@gmail.com>
2021-05-15 21:15:09 -0400

    feat(web): add ms time for nextjs to start the server

--- packages/web/server/src/index.ts
+++ packages/web/server/src/index.ts
@@ -11,6 +11,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev, conf: require('../../next.config.js') });
 const handle = app.getRequestHandler();
 
+const startTime = Date.now();
+
 (async () => {
   if (!process.env.NEXT_PUBLIC_BASE_URL) {
     console.error(`${chalkLog('error')} - NEXT_PUBLIC_BASE_URL is not defined in packages/web/.env`)
@@ -31,7 +33,7 @@ const handle = app.getRequestHandler();
 
     try {
       server.listen(PORT, () =>
-        console.error(`${chalkLog('success')} - Ready on http://localhost:${PORT} \n${chalkLog('success')} - API: http://localhost:${PORT}/graphql`)
+        console.error(`${chalkLog('success')} - Ready in ${Date.now() - startTime}ms on http://localhost:${PORT} \n${chalkLog('success')} - API: http://localhost:${PORT}/graphql`)
       );
     } catch (err) {
       if (err) throw err;
