edfbcf702705e5f96d331ec80aa53e0468e101b5
Alex Overstreet <47640630+alexover1@users.noreply.github.com>
2021-05-17 19:28:44 -0400

    fix(web): home page responsive bug (#240)

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Navbar />
-      <div className="mt-14 grid grid-cols-3 gap-14">
+      <div className="mt-14 grid grid-cols-1 md:grid-cols-3 gap-14">
         <div className="col-span-2 flex justify-center">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <Post />
@@ -14,7 +14,7 @@ const HomePage: React.FC = () => {
             <Post />
           </div>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="hidden md:flex flex-col items-center">
           <h2>Something on your mind?</h2>
           <button className="font-bold w-full md:w-1/3 bg-primary px-8 py-2 rounded-lg">
             Make a Post
