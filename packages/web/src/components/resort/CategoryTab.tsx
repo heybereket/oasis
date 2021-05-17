7778d0fd0852405bd83471d77e29341ab8ef51da
mooyg-alt <mmooyani@gmail.com>
2021-05-17 16:16:28 +0000

    fixing more stuff

--- packages/web/src/components/resort/CategoryTab.tsx
+++ packages/web/src/components/resort/CategoryTab.tsx
@@ -1,11 +1,10 @@
 const CategoryTab: React.FC = () => {
   return(
     <>
-    <div className={`bg-resortSecondary max-w-md mb-10 rounded-lg`}>
+    <div className={`bg-resortSecondary md:w-rsw md:max-w-md w-rsw  m-10 mb-10 h-rs rounded-lg`}>
       something
     </div>
     </>
   )
 }
 export default CategoryTab
-
--- packages/web/src/pages/resort/index.tsx
+++ packages/web/src/pages/resort/index.tsx
@@ -3,19 +3,22 @@ import CategoryTab from "@components/resort/CategoryTab";
 import React from "react";
 const Resort: React.FC = () => {
   return(
-      <div className="h-screen">
+      <>
       <Navbar/>
-        <div className={`flex-col flex mt-12 w-full md:pl-32 pl-1`} >
+        <div className={`flex-col flex  w-full md:pl-32 pl-1`} >
           <div>
             <h2>Resorts</h2>
       <span className={`text-secondary`}>Choose a category to explore resort.</span>
         </div>
           </div>
-          <div className={'md:pl-32 pl-1 flex-grow-1 flex-wrap'}>
+      <div className={'flex overflow-visible md:pl-32 pl-1 h-3/4 w-full  p-16 flex-wrap'}>
+          <CategoryTab/>
+          <CategoryTab/>
           <CategoryTab/>
           <CategoryTab/>
           </div>
-      </div>
+
+      </>
   )
 }
 
