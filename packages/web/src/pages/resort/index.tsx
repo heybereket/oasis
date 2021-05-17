d92d14b1e02b8b86a943cc66a46f7991e3f68b14
mooyg <mooybot@gmail.com>
2021-05-17 17:35:34 +0530

    /resort tried to make it more mobile responsive

--- packages/web/src/pages/resort/index.tsx
+++ packages/web/src/pages/resort/index.tsx
@@ -1,15 +1,22 @@
 import { Container } from "@components/common/Container";
 import { Navbar } from "@components/navbar/Navbar";
+import CategoryTab from "@components/resort/CategoryTab";
 import React from "react";
 const Resort: React.FC = () => {
   return(
       <>
       <Navbar/>
-      <Container >
-        <div className={`flex-col mt-20 w-full`} style={{paddingLeft: `6%`}}>
-      <h2>Resorts</h2>
+
+        <div className={`flex-col flex h-1/2 mt-12 w-full md:pl-32 pl-1`} >
+          <div>
+            <h2>Resorts</h2>
+      <span className={`text-secondary`}>Choose a category to explore resort.</span>
+          </div>
+          <div className={`mt-16 min-h-1/2`}>
+          <CategoryTab/>
+          </div>
         </div>
-      </Container>
+
       </>
   )
 }
