c8ad73e2bf42cd1b7a8727ad13ba4c23a82e74e6
Bereket Semagn <semagnbereket@gmail.com>
2021-05-17 11:29:24 -0400

    chore(web): remove js and tsx for tailwind purge

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   mode: 'jit',
   darkMode: 'class',
   purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
   ],
   theme: {
     fontFamily: {
