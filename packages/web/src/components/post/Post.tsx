09b8741a4215e5df48f900ed59981c2440e824f0
Alex Overstreet <47640630+alexover1@users.noreply.github.com>
2021-05-18 08:04:25 -0400

    feat(web): home page layout fix (#247)
    * fix(web): home page responsive bug
    
    * feat(web): added like count to post
    
    * feat(web): home page layout fix
    
    * fix(web): props not used lint error
    
    * fix(web): add closing tag for section
    
    Co-authored-by: Bereket Semagn <semagnbereket@gmail.com>

--- packages/web/src/components/post/Post.tsx
+++ packages/web/src/components/post/Post.tsx
@@ -9,7 +9,7 @@ interface PostProps {
 
 export const Post: React.FC<PostProps> = (props: PostProps) => {
   return (
-    <div className="shadow-lg max-w-[550px] w-full min-h-[220px] bg-gray-800 p-4 rounded-2xl flex flex-col justify-between">
+    <div className="shadow-lg max-w-[550px] w-full min-h-[220px] bg-gray-800 px-5 pt-2 pb-4 rounded-2xl flex flex-col justify-between">
       <div>
         <header className="flex items-center space-x-4">
           <div className="flex-none bg-gray-600 rounded-full w-11 h-11" />
@@ -33,8 +33,8 @@ export const Post: React.FC<PostProps> = (props: PostProps) => {
       </div>
       <footer className="flex justify-between">
         <p className="text-sm font-bold">9:44PM • May 17th 2021</p>
-        <div className="flex items-center">
-          <p className="text-sm">12 replies&nbsp;</p>
+        <div className="flex items-center space-x-2">
+          <p className="text-sm">12 replies</p>
           <Comments />
         </div>
       </footer>
