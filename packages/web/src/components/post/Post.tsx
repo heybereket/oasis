7a7198dfd5172eb6e4a5740214b6ab89c4b5ad14
Bereket Semagn <semagnbereket@gmail.com>
2021-05-18 07:40:47 -0400

    fix(web): add props to posts

--- packages/web/src/components/post/Post.tsx
+++ packages/web/src/components/post/Post.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Comments, SmallUpArrow, SmallDownArrow } from '@icons/index';
 
-export const Post: React.FC = () => {
+interface PostProps {
+  name: string;
+  username: string;
+  votes: number;
+}
+
+export const Post: React.FC<PostProps> = (props: PostProps) => {
   return (
     <div className="shadow-lg max-w-[550px] w-full min-h-[220px] bg-gray-800 p-4 rounded-2xl flex flex-col justify-between">
       <div>
@@ -9,12 +15,12 @@ export const Post: React.FC = () => {
           <div className="flex-none bg-gray-600 rounded-full w-11 h-11" />
           <div className="flex items-center justify-between w-full">
             <div>
-              <p className="text-lg font-bold">Alex</p>
-              <p className="-mt-1 text-gray-200 font-bold">@alexover1</p>
+              <p className="text-lg font-bold">{props.name}</p>
+              <p className="-mt-1 text-gray-200 font-bold">@{props.username}</p>
             </div>
             <div className="flex flex-col items-center">
               <SmallUpArrow />
-              <p className="font-bold bg-gray-700 px-2 rounded-full">23</p>
+              <p className="font-bold bg-gray-700 px-2 rounded-full">{props.votes}</p>
               <SmallDownArrow />
             </div>
           </div>
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -9,9 +9,26 @@ const HomePage: React.FC = () => {
       <div className="mt-14 grid grid-cols-1 md:grid-cols-3 gap-14">
         <div className="col-span-2 flex justify-center">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <Post />
-            <Post />
-            <Post />
+            <Post
+              name="Alex"
+              username="alex"
+              votes={500}
+            />
+            <Post
+              name="Bereket"
+              username="bereket"
+              votes={999}
+            />
+            <Post
+              name="Henry"
+              username="henry"
+              votes={230}
+            />
+            <Post
+              name="F1sh"
+              username="f1sh"
+              votes={95}
+            />
           </div>
         </div>
         <div className="hidden md:flex flex-col items-center">
