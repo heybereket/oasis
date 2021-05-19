7e7cf0958c6d440370a493cddae0af7e070be68e
Henry LeCompte <32208783+HenryLeC@users.noreply.github.com>
2021-05-19 07:00:19 -0400

    chore(web): Changed followers numbers to be more suitable

--- packages/web/src/pages/user/[username]/index.tsx
+++ packages/web/src/pages/user/[username]/index.tsx
@@ -117,11 +117,11 @@ const Profile: React.FC<ProfileProps> = (props) => {
               </div>
               <div className="mt-8 flex bg-gray-800 rounded-2xl py-4 justify-center gap-8">
                 <div className="flex flex-col text-center leading-4">
-                  <span className="text-2xl font-black">32</span>
+                  <span className="text-2xl font-black">666</span>
                   <span className="font-extrabold text-sm">Followers</span>
                 </div>
                 <div className="flex flex-col text-center leading-4">
-                  <span className="text-2xl font-black">22</span>
+                  <span className="text-2xl font-black">69</span>
                   <span className="font-extrabold text-sm">Posts</span>
                 </div>
                 <div className="flex flex-col text-center leading-4">
@@ -163,11 +163,11 @@ const Profile: React.FC<ProfileProps> = (props) => {
           </div>
           <div className="mt-6 flex justify-center gap-12 md:hidden">
             <div className="flex flex-col text-center leading-4">
-              <span className="text-2xl font-black">32</span>
+              <span className="text-2xl font-black">666</span>
               <span className="font-extrabold text-sm">Followers</span>
             </div>
             <div className="flex flex-col text-center leading-4">
-              <span className="text-2xl font-black">22</span>
+              <span className="text-2xl font-black">69</span>
               <span className="font-extrabold text-sm">Posts</span>
             </div>
             <div className="flex flex-col text-center leading-4">
