efa5d32977e5a46d9d4e5ed8827f6d82bff370c1
Henry LeCompte <henry@volunteerio.us>
2021-05-18 21:52:35 -0400

    feat: Add ability to like and dislike posts

--- packages/web/src/components/post/Post.tsx
+++ packages/web/src/components/post/Post.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Comments, SmallUpArrow, SmallDownArrow } from '@icons/index';
 import Link from 'next/link';
-import { PaginatePostsQuery } from '@oasis/client-gql';
+import {
+  PaginatePostsQuery,
+  useLikeDislikePostMutation,
+} from '@oasis/client-gql';
 
 type PostType = PaginatePostsQuery['paginatePosts'][0];
 
@@ -9,7 +12,39 @@ interface Props {
   post: PostType;
 }
 
+enum LikeDislikeState {
+  'LIKED',
+  'NONE',
+  'DISLIKED',
+}
+
 export const Post: React.FC<Props> = ({ post }) => {
+  const [likePost] = useLikeDislikePostMutation({
+    variables: {
+      like: true,
+      dislike: false,
+      postId: post.id,
+    },
+  });
+  const [dislikePost] = useLikeDislikePostMutation({
+    variables: {
+      like: false,
+      dislike: true,
+      postId: post.id,
+    },
+  });
+
+  const [likes, setLikes] = useState(post.likes);
+  const [dislikes, setDislikes] = useState(post.dislikes);
+
+  const [likeState, setLikeState] = useState<LikeDislikeState>(
+    post.isLiked
+      ? LikeDislikeState.LIKED
+      : post.isDisliked
+      ? LikeDislikeState.DISLIKED
+      : LikeDislikeState.NONE
+  );
+
   return (
     <div className="shadow-lg max-w-580 w-full bg-gray-800 px-5 pt-2 pb-4 rounded-2xl flex flex-col justify-between">
       <div>
@@ -30,11 +65,47 @@ export const Post: React.FC<Props> = ({ post }) => {
               </a>
             </Link>
             <div className="flex flex-col items-center">
-              <SmallUpArrow className={'cursor-pointer'} />
+              <SmallUpArrow
+                onClick={() => {
+                  likePost();
+                  if (likeState === LikeDislikeState.LIKED) {
+                    setLikeState(LikeDislikeState.NONE);
+                    setLikes(likes - 1);
+                  } else if (likeState === LikeDislikeState.DISLIKED) {
+                    setLikeState(LikeDislikeState.LIKED);
+                    setDislikes(dislikes - 1);
+                    setLikes(likes + 1);
+                  } else {
+                    setLikeState(LikeDislikeState.LIKED);
+                    setLikes(likes + 1);
+                  }
+                }}
+                className={`cursor-pointer ${
+                  likeState === LikeDislikeState.LIKED ? 'text-blue-400' : ''
+                }`}
+              />
               <p className="font-bold bg-gray-700 px-2 rounded-full">
-                {post.likes - post.dislikes}
+                {likes - dislikes}
               </p>
-              <SmallDownArrow className={'cursor-pointer'} />
+              <SmallDownArrow
+                onClick={() => {
+                  dislikePost();
+                  if (likeState === LikeDislikeState.DISLIKED) {
+                    setLikeState(LikeDislikeState.NONE);
+                    setDislikes(dislikes - 1);
+                  } else if (likeState === LikeDislikeState.LIKED) {
+                    setLikeState(LikeDislikeState.DISLIKED);
+                    setDislikes(dislikes + 1);
+                    setLikes(likes - 1);
+                  } else {
+                    setLikeState(LikeDislikeState.DISLIKED);
+                    setDislikes(dislikes + 1);
+                  }
+                }}
+                className={`cursor-pointer ${
+                  likeState === LikeDislikeState.DISLIKED ? 'text-blue-400' : ''
+                }`}
+              />
             </div>
           </div>
         </header>
