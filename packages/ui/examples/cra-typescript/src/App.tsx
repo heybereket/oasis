17688d1d85fa82f9ff35ae6a46ad8c7b881f2896
Samar Mohan <samar.mohan@gmail.com>
2021-05-20 13:28:51 -0500

    feat(ui): add cra-typescript example

---
+++ packages/ui/examples/cra-typescript/src/App.tsx
@@ -0,0 +1,13 @@
+import { SayHello } from "@oasis-sh/ui/";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name="Oasis" />
+    </div>
+  );
+}
+
+export default App;
