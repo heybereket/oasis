54495781723ad21f6761ae6c6aaad71798946ca3
Samar Mohan <samar.mohan@gmail.com>
2021-05-20 10:14:45 -0500

    feat(ui): add eslint and prettier

--- packages/ui/src/components/HelloWorld.tsx
+++ packages/ui/src/components/HelloWorld.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
+import * as React from "react";
 
 const HelloWorld: React.FC = () => {
   return (
     <div>
       <h1>Hello World!</h1>
     </div>
-  )
-}
+  );
+};
 
 export default HelloWorld;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,1 @@
-export { default as HelloWorld } from './components/HelloWorld';
+export { default as HelloWorld } from "./components/HelloWorld";
