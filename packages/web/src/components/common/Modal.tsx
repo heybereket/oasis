6e58b734a05dafbff2a03f5ccd347314930b390b
Simplyalex99 <alexmflores99@hotmail.ca>
2021-05-19 23:47:06 -0400

    Added re-usable Modal
    Called Modal Component in username/index.tsx file
    
    Changed old modal version EditModal to pure tailwind css.

--- packages/web/src/components/common/Modal.tsx
+++ packages/web/src/components/common/Modal.tsx
@@ -1,6 +1,4 @@
-import React, { useRef } from 'react';
-import { useOnClickOutside } from '@utils/hooks/useOnClickOutside';
-
+import React from 'react';
 interface Props {
   open: boolean;
   closeHandler: () => void;
@@ -8,28 +6,44 @@ interface Props {
 }
 
 export const Modal: React.FC<Props> = ({ open, closeHandler, children }) => {
-  if (typeof window === 'undefined' || !open) return null;
 
-  const modalRef = useRef() as React.MutableRefObject<HTMLInputElement>;
-  useOnClickOutside(modalRef, () => closeHandler());
+  if (!open) return null;
+
+  const checkClickedOnOutside = (e: any) => {
+    const modal = document.getElementById("modal") as HTMLElement;
+    if (e.target.id == "modal") {
+
+      modal.classList.add('hide');
+      closeHandler();
+
+    }
+
+  }
 
   return (
-    <div className="w-screen h-screen bg-black top-0 left-0 fixed flex justify-center items-center">
-      <div ref={modalRef} className="bg-gray-600">
-        <svg
-          onClick={() => closeHandler()}
-          className="h-6 w-6 cursor-pointer p-1 hover:bg-gray-500 rounded-full"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          />
-        </svg>
+    <div id="modal" onClick={(e) => checkClickedOnOutside(e)} className="w-screen z-50 h-screen bg-black bottom-0 right-0 top-0 left-0 fixed flex justify-center items-center">
+      <div>
+        <span className="flex justify-between">
+
+          <span></span>
+          <svg
+            onClick={() => closeHandler()}
+            className=" h-6 w-6 cursor-pointer p-1 hover:bg-gray-500 rounded-full"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+
         {children}
+
       </div>
+
     </div>
   );
-};
+}
