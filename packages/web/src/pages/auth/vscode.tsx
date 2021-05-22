29772492b8c7a79fae5617c975753cac83e4e41f
vishy-dev <agarwal.vrishank@gmail.com>
2021-05-22 11:42:28 +0100

    fix(web): fix vscode auth page (no gui yet) and add apiBaseURL

--- packages/web/src/pages/auth/vscode.tsx
+++ packages/web/src/pages/auth/vscode.tsx
@@ -1,18 +1,41 @@
-import { baseURL } from '@lib/constants';
-import Link from 'next/link';
+import { Button } from '@components/common/Button';
+import { apiBaseURL } from '@lib/constants';
+import { Login } from '@lib/login';
+import { useGetCurrentUserQuery } from '@oasis-sh/client-gql';
+import { useEffect, useState } from 'react';
 
 const authTypes = ['github', 'discord', 'google', 'twitter'];
 
 export default function VSCodeAuth(): any {
-  return (
+  const { data, loading } = useGetCurrentUserQuery();
+  const [done, setDone] = useState(false);
+
+  useEffect(() => {
+    if (data)
+      fetch(`${apiBaseURL}/auth/vscode`, {
+        method: 'POST',
+        headers: {
+          authorization: 'Bearer ' + window.location.search.replace('?t=', ''),
+        },
+      }).then(() => setDone(true));
+  }, [data]);
+
+  if (loading) return <h1 className="text-3xl">Loading...</h1>;
+
+  if (data && !done)
+    return <h1 className="text-3xl">Hooking you up with VSCode...</h1>;
+
+  return done ? (
+    <h1 className="text-3xl">Done! You can close this browser tab now</h1>
+  ) : (
     <div>
       <h1 className="text-3xl">VSCode Oasis Auth</h1>
 
       {authTypes.map((provider) => {
         return (
-          <Link href={`${baseURL}/api/auth/${provider}`} key={provider}>
-            <a>Login with {provider}</a>
-          </Link>
+          <Button key={provider} onClick={() => Login(provider)}>
+            Login with {provider}
+          </Button>
         );
       })}
     </div>
