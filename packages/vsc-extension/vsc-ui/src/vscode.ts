dab049b86356cff2e156e75577d21b4e7788fda7
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-05-22 18:37:41 +0100

    refactor(vsce): Change aqcuiring vscode login

--- packages/vsc-extension/vsc-ui/src/vscode.ts
+++ packages/vsc-extension/vsc-ui/src/vscode.ts
@@ -1,9 +1,1 @@
-const vsCodeFunction = Function(`
-  // forgive me for my sins
-  if (typeof acquireVsCodeApi == 'function') {
-    return acquireVsCodeApi();
-  } else {
-    return undefined;
-  }
-  `);
-export const vscode = vsCodeFunction();
+export const vscode = (globalThis as any).acquireVsCodeApi();
