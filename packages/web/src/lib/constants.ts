99f07668abc6dd053bc24fc86991888ec461af88
Bereket Semagn <semagnbereket@gmail.com>
2021-05-22 20:38:49 -0400

    fix(ui): use a href instead of window.location

--- packages/web/src/lib/constants.ts
+++ packages/web/src/lib/constants.ts
@@ -8,8 +8,5 @@ export const localBaseURL = process.env.NEXT_PUBLIC_BASE_URL;
 export const gqlBaseURL = `${baseURL}/graphql`;
 export const apiBaseURL = `${baseURL}/api`;
 
-export const linkRegex =
-  /(^|\s)(https?:\/\/)(www\.)?([-a-z0-9]{1,63}\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\.[a-z]{1,6}(\/[-\\w@\\+\\.~#\\?&/=%]*)?[^\s()]+/;
+export const linkRegex = /(^|\s)(https?:\/\/)(www\.)?([-a-z0-9]{1,63}\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\.[a-z]{1,6}(\/[-\\w@\\+\\.~#\\?&/=%]*)?[^\s()]+/;
 export const mentionRegex = /^(?!.*\bRT\b)(?:.+\s)?#?@\w+/i;
-
-export const undefinedWindow = typeof window === 'undefined';
