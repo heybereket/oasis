4ad17608fdd4782d1600f8e206723204fb271974
Bereket Semagn <semagnbereket@gmail.com>
2021-05-26 18:48:45 -0400

    chore(web): add meta description to _document

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -9,6 +9,10 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#5C91FC" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="icon" href="/static/favicons/favicon.ico" />
+          <meta
+            name="Description"
+            content="The social platform for developers"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
