345a1dba4a930bbe4d3338154b3325bceaac259a
Henry LeCompte <32208783+HenryLeC@users.noreply.github.com>
2021-05-27 14:47:01 -0400

    Revert "refactor: simplified index.tsx (#317)"
    This reverts commit 5942cc7becefacac7262571712fd7d5d2dfd6c7d.

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -22,13 +22,6 @@ import {
   GetCurrentUserDocument,
   useDeletePostMutation,
 } from '@oasis-sh/client-gql';
-import {
-  FollowUserSection,
-  FriendActivitySection,
-  PostsSection,
-  ProfileSection,
-  TrendingSection
-} from '../components/home/index'
 import StyledMarkdown from 'src/markdown/StyledMarkdown';
 import { login, logout } from '@lib/login';
 import { SEO } from '@utils/SEO';
@@ -71,23 +64,128 @@ const HomePage: React.FC<IndexPageProps> = ({ vars }) => {
         <div className="z-10 relative px-6 grid grid-cols-1 lg:grid-cols-three gap-16">
           <div className="hidden lg:flex flex-col flex-1 sticky top-28 h-px">
             <div className="flex-shrink-0 w-full flex flex-col py-6 px-8 bg-gray-800 rounded-2xl">
-              <ProfileSection user={user} currentUserLoading={currentUserLoading} StyledMarkdown={StyledMarkdown}></ProfileSection>
+              {currentUserLoading || (
+                <>
+                  <div className="flex items-center space-x-4">
+                    <img
+                      src={user?.avatar}
+                      alt="avatar"
+                      className="w-14 h-14 rounded-full"
+                    />
+                    <div>
+                      <p className="font-bold text-xl">{user?.name}</p>
+                      <p className="font-bold text-light -mt-1">
+                        @{user?.username}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="mt-3">
+                    {user?.bio !== null ? (
+                      <StyledMarkdown text={user?.bio ?? ''} isBio={true} />
+                    ) : (
+                      'Your bio has not been set.'
+                    )}
+                  </p>
+                  <Link href={`/user/${user?.username}`}>
+                    <a className="flex items-center space-x-0.5 mt-2">
+                      <p className="font-bold text-lg text-primary">
+                        View Profile
+                      </p>
+                      <RightArrow className="text-primary" />
+                    </a>
+                  </Link>
+                </>
+              )}
             </div>
-            <FriendActivitySection></FriendActivitySection>
+            <Sidebar title="Friends Activity" className="mt-10">
+              <div className="flex-shrink-0 mt-6 flex flex-col space-y-4">
+                <FriendActivity
+                  name="Sam Jakob"
+                  activity={['Playing', 'Visual Studio Code']}
+                />
+                <FriendActivity
+                  name="Angshu31"
+                  activity={['Listening to', 'Spotify']}
+                />
+                <FriendActivity
+                  name="bereket"
+                  activity={['Browsing', 'the Feed']}
+                />
+                <FriendActivity
+                  name="Henry"
+                  activity={['Idle for', '10 minutes']}
+                />
+              </div>
+            </Sidebar>
           </div>
           <div className="flex flex-col flex-1 w-full space-y-12 pb-12 mt-[33px]">
-            <PostsSection
-              StyledMarkdown={StyledMarkdown}
-              user={user}
-              posts={posts}
-              createPost={createPost}
-              likeDislikePost={likeDislikePost}>
-            </PostsSection>
+            {user && (
+              <CreatePostInput
+                avatarUrl={user.avatar}
+                onSubmit={(value: string) =>
+                  createPost({ variables: { message: value, topics: [] } })
+                }
+              />
+            )}
+            {[...posts].reverse().map((post: any, index: number) => (
+              <Post
+                post={post}
+                key={index}
+                markdown={(text) => {
+                  return <StyledMarkdown text={text} isPost={true} />;
+                }}
+                currentUser={user}
+                deletePost={(id) => {
+                  deletePost({
+                    variables: {
+                      postId: id,
+                    },
+                  });
+                  window.location.reload();
+                }}
+                likePost={() => {
+                  likeDislikePost({
+                    variables: {
+                      postId: post.id,
+                      dislike: false,
+                      like: true,
+                    },
+                  });
+                }}
+                dislikePost={() => {
+                  likeDislikePost({
+                    variables: {
+                      postId: post.id,
+                      dislike: true,
+                      like: false,
+                    },
+                  });
+                }}
+              />
+            ))}
           </div>
           <div className="hidden lg:flex flex-col flex-1 sticky top-28 h-px">
             <div className="w-full flex flex-col items-center">
-              <TrendingSection></TrendingSection>
-              <FollowUserSection></FollowUserSection>
+              <Sidebar title="Trending on Oasis">
+                <div className="mt-6">
+                  <TopicBadge content="JavaScript" />
+                  <TopicBadge content="Python" />
+                  <TopicBadge content="Node.js" />
+                  <TopicBadge content="Artificial Intelligence" />
+                  <TopicBadge content="TypeScript" />
+                  <TopicBadge content="Memes" />
+                  <TopicBadge content="Programming" />
+                  <TopicBadge content="Code" />
+                  <TopicBadge content="CatsWhoCode" />
+                </div>
+              </Sidebar>
+              <Sidebar title="Find New People" className="mt-10">
+                <div className="mt-6 space-y-3">
+                  <FollowUser name="Bereket" username="heybereket" />
+                  <FollowUser name="Alex" username="alexover1" />
+                  <FollowUser name="Sam" username="samjakob" />
+                </div>
+              </Sidebar>
             </div>
           </div>
         </div>
