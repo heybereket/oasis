bbf6c48ba9b61a86f8e820b4f5e3b87d315ad678
Henry LeCompte <henry@volunteerio.us>
2021-05-27 20:00:27 -0400

    fix(web): Removed next-offline (it was breaking everything)

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -3,56 +3,51 @@ const withOffline = require('next-offline');
 const { createSecureHeaders } = require('next-secure-headers');
 const { join } = require('path');
 
-const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = withBundleAnalyzer(
-  withOffline({
-    target: 'serverless',
-    dontAutoRegisterSw: true,
-    workboxOpts: {
-      swDest: '.next',
-    },
-    future: {
-      webpack5: true,
-    },
-    poweredByHeader: false,
-    async headers() {
-      return [{ source: '/(.*)', headers: createSecureHeaders() }];
-    },
-    async redirects() {
-      return [
-        {
-          source: '/u/:username',
-          destination: '/user/:username',
-          permanent: true,
-        },
-        {
-          source: '/r/:resort',
-          destination: '/resort/:resort',
-          permanent: true,
-        },
-        {
-          source: '/oss/github',
-          destination: 'https://github.com/oasis-sh/oasis',
-          permanent: false,
-        },
-      ];
-    },
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      // if (!isServer) {
-      //   config.node = {
-      //     fs: 'empty',
-      //   };
-      // }
+module.exports = withBundleAnalyzer({
+  future: {
+    webpack5: true,
+  },
+  poweredByHeader: false,
+  async headers() {
+    return [{ source: '/(.*)', headers: createSecureHeaders() }];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/u/:username',
+        destination: '/user/:username',
+        permanent: true,
+      },
+      {
+        source: '/r/:resort',
+        destination: '/resort/:resort',
+        permanent: true,
+      },
+      {
+        source: '/oss/github',
+        destination: 'https://github.com/oasis-sh/oasis',
+        permanent: false,
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    // if (!isServer) {
+    //   config.node = {
+    //     fs: 'empty',
+    //   };
+    // }
 
-      return config;
-    },
-    env: {
-      PROJECT_ROOT: join(__dirname, '../..'),
-      IS_NEXT: true,
-      OASIS_API_SRC_PATH: join(__dirname, '../api/src'),
-      API_MODE: process.env.API_MODE === 'remote' ? 'remote' : 'local',
-    },
-  })
-);
+    return config;
+  },
+  env: {
+    PROJECT_ROOT: join(__dirname, '../..'),
+    IS_NEXT: true,
+    OASIS_API_SRC_PATH: join(__dirname, '../api/src'),
+    API_MODE: process.env.API_MODE === 'remote' ? 'remote' : 'local',
+  },
+});
