e46ff41d35ae9f7d95e7f3e8d81925d8162e40a8
Henry LeCompte <henry@volunteerio.us>
2021-05-28 17:55:09 -0400

    feat(docker): Checked in ormconfig
    Maybe I should stop pushing public docker images with secrets in them lol

---
+++ docker/docker-ormconfig.ts
@@ -0,0 +1,14 @@
+import { ConnectionOptions } from 'typeorm';
+import { joinRoot } from '@utils/common/rootPath';
+
+export const ormconfig: ConnectionOptions = {
+  type: process.env.DATABASE_TYPE as any,
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  uuidExtension: 'uuid-ossp',
+  entities: [joinRoot('./entities/*.*')],
+  migrations: [joinRoot('./migrations/*.*')],
+};
+
+export default ormconfig;
