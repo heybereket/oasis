82cee6f59b31e76237d43e33df069b226b326745
Bereket Semagn <semagnbereket@gmail.com>
2021-05-27 20:32:26 -0400

    fix(web): put headers in an array

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -12,37 +12,42 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   headers() {
     return [
-      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
       {
-        key: 'Referrer-Policy',
-        value: 'origin-when-cross-origin',
-      },
-      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-      {
-        key: 'X-Frame-Options',
-        value: 'DENY',
-      },
-      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-      {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff',
-      },
-      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-      {
-        key: 'X-DNS-Prefetch-Control',
-        value: 'on',
-      },
-      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=31536000; includeSubDomains; preload',
-      },
-      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-      // Opt-out of Google FLoC: https://amifloced.org/
-      {
-        key: 'Permissions-Policy',
-        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
-      },
+        source: '/(.*)',
+        headers: [
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+          // Opt-out of Google FLoC: https://amifloced.org/
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+        ],
+      }
     ];
   },
   async redirects() {
@@ -81,3 +86,37 @@ module.exports = withBundleAnalyzer({
     API_MODE: process.env.API_MODE === 'remote' ? 'remote' : 'local',
   },
 });
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+];
