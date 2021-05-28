d2b66b8a7879e754bf43d5d4bd6a5e82b5b132f0
Bereket Semagn <semagnbereket@gmail.com>
2021-05-27 20:27:05 -0400

    chore(web): add more headers to next config

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -12,21 +12,36 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   headers() {
     return [
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
       {
-        source: '/',
-        headers: [{ key: 'X-Frame-Options', value: 'SAMEORIGIN' }],
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
       },
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
       {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-          {
-            key: 'Feature-Policy',
-            value: "geolocation 'self'; microphone 'self'; camera 'self'",
-          },
-        ],
+        key: 'X-Frame-Options',
+        value: 'DENY',
+      },
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+      {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+      },
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+      },
+      // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+      // Opt-out of Google FLoC: https://amifloced.org/
+      {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
       },
     ];
   },
