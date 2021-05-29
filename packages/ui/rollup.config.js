773a703678660b153b1e820d8480a43f1958f2ce
Bereket Semagn <semagnbereket@gmail.com>
2021-05-29 16:25:44 -0400

    chore(ui): remove postcss and tailwind as deps

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -2,7 +2,6 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
 import json from "@rollup/plugin-json";
 const packageJson = require("./package.json");
 
@@ -25,7 +24,6 @@ export default {
     resolve(),
     commonjs(),
     typescript(),
-    postcss(),
     json(),
   ],
 };
