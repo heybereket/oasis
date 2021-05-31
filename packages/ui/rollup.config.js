ac676bc977ed59500414b9d4338b24efd24873ed
Bereket Semagn <semagnbereket@gmail.com>
2021-05-30 20:36:04 -0400

    chore(ui): add cjs back

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -9,6 +9,11 @@ export default {
   input: 'src/index.ts',
   output: [
     {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
       file: packageJson.module,
       format: 'esm',
       sourcemap: true,
