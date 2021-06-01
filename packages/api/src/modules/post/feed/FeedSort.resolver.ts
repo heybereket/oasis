8cbdc50c0bf838bdcb3a1f24ebc9428ddf3d0e07
Henry LeCompte <henry@volunteerio.us>
2021-06-01 19:58:05 -0400

    fix(api): Fixed feed sort to take into account dislikes

--- packages/api/src/modules/post/feed/FeedSort.resolver.ts
+++ packages/api/src/modules/post/feed/FeedSort.resolver.ts
@@ -9,17 +9,19 @@ export class DeletePostResolver {
     @Arg('offset') offset: number
   ) {
     const dataIds = Post.query(
-      `SELECT post.id, COUNT(user_liked_posts_post."userId") AS likesCount
+      `SELECT post.id,
+    COUNT(user_liked_posts_post."userId") - COUNT(user_disliked_posts_post."userId") AS likesTotal
+
     FROM post
 
     LEFT JOIN user_liked_posts_post
     ON post.id = user_liked_posts_post."postId"
 
-    LEFT JOIN "user"
-    ON post."authorId" = "user"."id"
+    LEFT JOIN user_disliked_posts_post
+    ON post.id = user_disliked_posts_post."postId"
 
-    GROUP BY post.id, "user"."id"
-    ORDER BY likesCount DESC, post."createdAt" DESC
+    GROUP BY post.id
+    ORDER BY likesTotal DESC, post."createdAt" DESC
     LIMIT $1 OFFSET $2;`,
       [limit, offset]
     );
