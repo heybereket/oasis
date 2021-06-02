3a7ef04f7b4410a9ed45f10bdb72bbaf83262760
Bereket Semagn <semagnbereket@gmail.com>
2021-06-02 13:05:04 -0400

    fix(web): move maintainer data to json and fix build error

--- packages/web/src/pages/oss/core.tsx
+++ packages/web/src/pages/oss/core.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar, TeamMember } from '@oasis-sh/ui';
 import { useGetCurrentUser } from '@lib/common/getCurrentUser';
 import { login, logout } from '@lib/auth/login';
-
+import maintainers from '../../data/maintainers.json';
 export const CoreTeamPage: React.FC = () => {
   const { user, currentUserLoading } = useGetCurrentUser();
 
@@ -21,48 +21,16 @@ export const CoreTeamPage: React.FC = () => {
         </p>
       </div>
       <div className="flex flex-row justify-center flex-wrap mt-7 mx-auto max-w-7xl">
-        <TeamMember
-          avatar="https://avatars.githubusercontent.com/u/68391329?v=4"
-          bio="Building @oasis-sh — the social platform for developers 💻"
-          githubUsername="heybereket"
-          name="Bereket Semagn"
-          role="Founder & Maintainer"
-        />
-        <TeamMember
-          avatar="https://avatars.githubusercontent.com/u/32208783?v=4"
-          bio="A passionate 14 year old software engineer"
-          githubUsername="HenryLeC"
-          name="Henry LeCompte"
-          role="Maintainer"
-        />
-        <TeamMember
-          avatar="https://avatars.githubusercontent.com/u/51079288?v=4"
-          bio=""
-          githubUsername="Angshu31"
-          name="Angshu31"
-          role="Maintainer"
-        />
-        <TeamMember
-          avatar="https://avatars.githubusercontent.com/u/37072691?v=4"
-          bio=""
-          githubUsername="SamJakob"
-          name="SamJakob"
-          role="Maintainer & Designer"
-        />
-        <TeamMember
-          avatar="https://avatars.githubusercontent.com/u/56125930?v=4"
-          bio="I'm working on random, interesting open-source projects!"
-          githubUsername="f1shy-dev"
-          name="f1shy-dev"
-          role="Maintainer"
-        />
-        <TeamMember
-          avatar="https://cdn.discordapp.com/avatars/414459528998813736/285eb40416db5130c2a5aeb20b5f04fa.webp?size=256"
-          bio=""
-          githubUsername="BronzW"
-          name="BronzW"
-          role="Maintainer"
-        />
+        {maintainers.map((maintainer: any, key: any) => (
+          <TeamMember
+            key={key}
+            avatar={maintainer.avatar}
+            bio={maintainer.bio}
+            githubUsername={maintainer.github}
+            name={maintainer.name}
+            role={maintainer.role}
+          />
+        ))}
       </div>
     </>
   );
