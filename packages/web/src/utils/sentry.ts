8151580c0bb77c7633293735078c6111e94410a2
Bereket Semagn <semagnbereket@gmail.com>
2021-06-02 13:43:46 -0400

    chore(web): remove sentry enabled field

--- packages/web/src/utils/sentry.ts
+++ packages/web/src/utils/sentry.ts
@@ -3,10 +3,11 @@ import { Integrations } from '@sentry/tracing';
 import { isProduction } from '@lib/constants';
 
 export const initSentry = () => {
-  Sentry.init({
-    enabled: isProduction,
-    dsn: process.env.SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-    release: process.env.SENTRY_RELEASE,
-  });
+  if (isProduction) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      integrations: [new Integrations.BrowserTracing()],
+      release: process.env.SENTRY_RELEASE,
+    });
+  }
 };
