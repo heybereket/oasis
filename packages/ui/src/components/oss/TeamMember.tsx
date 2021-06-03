4ae784e983d16504a431db17da1e8f565d70cb4c
TheHackerCoding <29006605+TheHackerCoding@users.noreply.github.com>
2021-06-02 20:58:15 -0400

    feat(ui): add link to github

--- packages/ui/src/components/oss/TeamMember.tsx
+++ packages/ui/src/components/oss/TeamMember.tsx
@@ -12,7 +12,9 @@ type TeamMemberProps = {
 export const TeamMember: React.FC<TeamMemberProps> = (props) => {
   return (
     <div className="flex flex-col items-center w-[33%]">
-      <img src={props.avatar} className="rounded-full w-40 cursor-pointer" alt={props.name} onClick={props.onClick}/>
+      <a href={'https://github.com/' + props.github}>
+        <img src={props.avatar} className="rounded-full w-40 cursor-pointer" alt={props.name} onClick={props.onClick}/>
+      </a>
         <h5 className="font-bold mt-4 mb-3">{props.name}</h5>
         <p className="uppercase text-blue-500">{props.role}</p>
         <p className="text-gray-100 text-center">{props.bio}</p>
