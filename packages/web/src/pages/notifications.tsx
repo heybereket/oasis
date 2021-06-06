8a8ff09133919fd719f45d860b4d0e1c3313dc6d
Bereket Semagn <semagnbereket@gmail.com>
2021-06-05 20:56:27 -0400

    chore(web): temp remove modules

--- packages/web/src/pages/notifications.tsx
+++ packages/web/src/pages/notifications.tsx
@@ -1,3 +1,54 @@
-import { NotificationsPage } from '@modules/notifications/NotificationsPage';
+import {
+  GetCurrentUserDocument,
+  UpdateProfileInput,
+  useGetCurrentUserQuery,
+} from '@oasis-sh/react-gql';
+import { GetServerSideProps } from 'next';
+import { ssrRequest } from '@lib/common/ssrRequest';
+import { Navbar } from '@oasis-sh/ui';
+import React from 'react';
+import { useGetCurrentUser } from '@lib/common/getCurrentUser';
+import { login, logout } from '@lib/auth/login';
 
-export default NotificationsPage;
+interface NotificationPageProps {
+  initialApolloState: any;
+}
+
+const NotificationPage: React.FC<NotificationPageProps> = () => {
+  const origData = useGetCurrentUserQuery().data?.currentUser;
+  const initialValues: UpdateProfileInput = {
+    avatar: origData?.avatar,
+    bio: origData?.bio,
+    banner: origData?.banner,
+    name: origData?.name,
+    username: origData?.username,
+  };
+
+  const { user, currentUserLoading } = useGetCurrentUser();
+
+  return (
+    <>
+      <Navbar
+        user={user}
+        currentUserLoading={currentUserLoading}
+        login={login}
+        logout={logout}
+      />
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<NotificationPageProps> =
+  async ({ req }) => {
+    return {
+      props: {
+        initialApolloState: await ssrRequest(req, [
+          {
+            document: GetCurrentUserDocument,
+          },
+        ]),
+      },
+    };
+  };
+
+export default NotificationPage;
