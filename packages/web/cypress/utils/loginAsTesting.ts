4ff0bcdf3bf707bf9d0d4d0635304bdda703613c
Bereket Semagn <semagnbereket@gmail.com>
2021-06-06 11:49:51 -0400

    fix(cypress): build error

--- packages/web/cypress/utils/loginAsTesting.ts
+++ packages/web/cypress/utils/loginAsTesting.ts
@@ -1,3 +1,5 @@
+// <reference types="Cypress" />
+
 export const loginWithTesting = (username = 'testing') => {
   cy.intercept('localhost:3000', (req) => {
     req.headers.authorization = `TESTING ${username}`;
