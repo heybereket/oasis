a3e7585bffdd1fd95598dd5604e0e1de32ecfaf5
Jake Harper <jake@krunchy.net>
2021-06-08 14:47:45 -0700

    feat(ui): set container to same width as nav bar

--- packages/ui/src/components/shared/Container.tsx
+++ packages/ui/src/components/shared/Container.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Container: React.FC = ({ children }) => {
-  return <div className={`lg:max-w-lg mx-auto`}>{children}</div>;
+  return <div className={`lg:max-w-nav mx-auto`}>{children}</div>;
 };
