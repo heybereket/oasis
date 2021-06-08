0cc2b9371d0a08c09dec4c4f6b40aab5e8108dfc
BronzW <carlhugo.rydman@gmail.com>
2021-06-08 13:36:02 +0200

    feat(api): changed authorized role in getqueue to admin

--- packages/api/src/modules/reports/GetQueue.resolver.ts
+++ packages/api/src/modules/reports/GetQueue.resolver.ts
@@ -4,7 +4,7 @@ import { Authorized, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class GetQueueResolver {
-  @Authorized(Role.Moderator)
+  @Authorized(Role.Admin)
   @Query(() => [Report])
   async getQueue() {
     return await Report.find({
