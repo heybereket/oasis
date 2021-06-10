4a4db4a3b899a3e256c82d3d3b4d6bba66604285
Bereket Semagn <semagnbereket@gmail.com>
2021-06-09 20:02:36 -0400

    feat(ui): change rollup config file to typescript

--- packages/ui/rollup.config.ts
+++ packages/ui/rollup.config.ts
@@ -3,15 +3,20 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       sourcemap: true,
     },
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), json()],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    json()],
 };
