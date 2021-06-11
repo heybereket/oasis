b52dc76fbbfde1a3ca376e9b2fabc0b5b8a7378c
Henry LeCompte <henry@volunteerio.us>
2021-06-11 17:30:44 -0400

    feat(api): Added [NO-BAN] ovveride
    To override banning on mutations add `{ description: '[NO-BAN]' }` to the mutation decorator

--- packages/api/src/middleware/NotBanned.ts
+++ packages/api/src/middleware/NotBanned.ts
@@ -1,13 +1,22 @@
 import { MiddlewareFn } from 'type-graphql';
 import { ContextType } from '@root/apolloServer';
 import User from '@entities/User';
+import { ApolloError } from 'apollo-server-errors';
 
 export function NotBanned(force: boolean): MiddlewareFn {
   return async ({ context, info }, next) => {
     const user: User = await (context as ContextType).getUser();
-    if (info.operation.operation === 'mutation' || force) {
+
+    const operationDescription =
+      info.parentType.getFields()[info.fieldName].description;
+
+    if (
+      (info.operation.operation === 'mutation' &&
+        !(operationDescription ?? '').includes('[NO-BAN]')) ||
+      force
+    ) {
       if (Number.parseInt(user.banExiration ?? '0') > Date.now()) {
-        throw new Error('Sorry, you are banned');
+        throw new ApolloError('Sorry, you are banned', 'BANNED_USER');
       }
     }
 
