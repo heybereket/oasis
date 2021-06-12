eb1c2f7d61096308c1ce67fe23bb11a65ba11a2a
Bereket Semagn <semagnbereket@gmail.com>
2021-06-12 10:47:18 -0400

    chore(api): move await getDatabase() to top of file

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -18,6 +18,7 @@ config();
 export const initializeServer = async () => {
   try {
     await checkEnv();
+    await getDatabase();
     checkNodeMajor(15);
 
     const app = express();
@@ -26,8 +27,6 @@ export const initializeServer = async () => {
     if (process.env.OASIS_API_TRUST_PROXY === 'true') {
       app.set('trust proxy', 1);
     }
-
-    await getDatabase();
 
     const apolloServer = await createApolloServer();
 
