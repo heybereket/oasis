a4ae20c35f6c85cc29e628e9983c855ab838cf6e
Bereket Semagn <semagnbereket@gmail.com>
2021-06-12 09:37:50 -0400

    chore(api): move process.env.TESTING if else into 1 statement

--- packages/api/src/services/database.ts
+++ packages/api/src/services/database.ts
@@ -7,18 +7,15 @@ export const getDatabase = async () => {
   let ormconfig: ConnectionOptions;
 
   if (process.env.TESTING === 'true') {
+    seedDatabase();
     ormconfig = {
       type: 'sqlite',
       database: 'testing.sqlite',
       entities: [joinRoot('./entities/*.*')],
       synchronize: true,
-  };
+    };
   } else {
     ormconfig = require('@root/ormconfig').default;
-  }
-
-  if (process.env.TESTING === 'true') {
-    seedDatabase();
   }
 
   try {
