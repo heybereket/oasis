8264d58ae720353c7edfa4703cec1537bf18a82f
Bereket Semagn <semagnbereket@gmail.com>
2021-06-14 16:34:27 -0400

    feat(web): add scrollbar and typography tailwind plugins

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,2 @@
-export * from './format/date';
-export * from './format/number';
+export * from './utils/format/date';
+export * from './utils/format/number';
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -103,5 +103,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/typography')
+  ],
 };
