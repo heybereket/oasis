fef34285ae9d562ba2044ef723bb69307e1ea66b
Clayton Jensen <cj.krysk+github@protonmail.com>
2021-06-18 20:33:07 -0500

    fix conflicts

--- packages/ui/src/components/home/ProfileSection.tsx
+++ packages/ui/src/components/home/ProfileSection.tsx
@@ -41,19 +41,10 @@ export const ProfileSection: React.FC<Props> = ({
             className="flex items-center space-x-0.5 mt-2"
             href={`/u/${user?.username}`}
           >
-<<<<<<< HEAD
             <div className="flex flex-row font-bold text-lg text-primary">
               <p>{t('profileCard.view')}</p>
               <RightArrow />
             </div>
-=======
-            <>
-              <p className="font-bold text-lg text-primary">
-                {t('profileCard.view')}
-              </p>
-              <RightArrow className="text-primary" />
-            </>
->>>>>>> 7fce666a92af237b071febcfa189221276be7198
           </CustomLink>
         </div>
       )}
