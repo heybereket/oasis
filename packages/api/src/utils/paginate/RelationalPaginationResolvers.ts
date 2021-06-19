fb8d48fea8b56a481f77bce41605dfad3befaf88
Henry LeCompte <henry@volunteerio.us>
2021-06-19 19:15:26 -0400

    feat(api): Added option sort column and type filter to paginations queries

--- packages/api/src/utils/paginate/RelationalPaginationResolvers.ts
+++ packages/api/src/utils/paginate/RelationalPaginationResolvers.ts
@@ -3,6 +3,7 @@ import { Arg, FieldResolver, Resolver, Root } from 'type-graphql';
 import { BaseEntity } from 'typeorm';
 import { PaginatedResponse } from './PaginationResponse';
 import { __decorate, __param, __metadata } from 'tslib';
+import { ApolloError } from 'apollo-server-express';
 
 let a = 0;
 let b = 0;
@@ -22,16 +23,22 @@ for (const [getTargetEntity, obj] of mapping) {
     ResolverClass.prototype[funcName] = async (
       obj: BaseEntity & { id: string },
       limit: number,
-      offset: number
+      offset: number,
+      sortCol: string | undefined,
+      sortType: string | undefined
     ) => {
       const valEntity = getValEntity();
+
+      if (sortType && sortType !== 'ASC' && sortType !== 'DESC') {
+        throw new ApolloError('Sort type must be "DESC" or "ASC"');
+      }
 
       const query = valEntity
         .createQueryBuilder('root')
         .innerJoin(`root.${otherSideKey}`, 'vals', 'vals.id = :id', {
           id: obj.id,
         })
-        .orderBy(`root.id`);
+        .orderBy(`root.${sortCol ?? 'id'}`, sortType ?? 'ASC');
 
       const items = await query.skip(offset).take(limit).getMany();
       const total = await query.getCount();
@@ -57,6 +64,14 @@ for (const [getTargetEntity, obj] of mapping) {
         __param(0, Root()),
         __param(1, Arg('limit')),
         __param(2, Arg('offset')),
+        __param(
+          3,
+          Arg('sortCol', () => String, { nullable: true })
+        ),
+        __param(
+          4,
+          Arg('sortType', () => String, { nullable: true })
+        ),
         __metadata('design:type', Function),
         __metadata('design:paramtypes', [Object, Number, Number]),
         __metadata('design:returntype', Promise),
