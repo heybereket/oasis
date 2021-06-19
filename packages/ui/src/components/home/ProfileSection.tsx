2523dffd0d7008b9ed92582699f71729cf0b59fe
Clayton Jensen <cj.krysk+github@protonmail.com>
2021-06-18 20:14:57 -0500

    fix(ui): ui tweaks
    fixed small issues in ui, added new TabMeta component to provide styling to profile tabs

--- packages/ui/src/components/home/ProfileSection.tsx
+++ packages/ui/src/components/home/ProfileSection.tsx
@@ -41,10 +41,10 @@ export const ProfileSection: React.FC<Props> = ({
             className="flex items-center space-x-0.5 mt-2"
             href={`/u/${user?.username}`}
           >
-            <>
-              <p className="font-bold text-lg text-primary">{t('profileCard.view')}</p>
-              <RightArrow className="text-primary" />
-            </>
+            <div className="flex flex-row font-bold text-lg text-primary">
+              <p>{t('profileCard.view')}</p>
+              <RightArrow />
+            </div>
           </CustomLink>
         </div>
       )}
