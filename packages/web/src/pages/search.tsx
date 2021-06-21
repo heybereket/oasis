70eaf9d164a4d7e6afb633d3da240a8cf533e08d
Bereket Semagn <semagnbereket@gmail.com>
2021-06-21 09:08:42 -0400

    fix(global): build errors

--- packages/web/src/pages/search.tsx
+++ packages/web/src/pages/search.tsx
@@ -11,7 +11,7 @@ import {
   useSearchQuery,
 } from '@oasis-sh/react-gql';
 import { LargeUserCard, Navbar, Post, User as UserCard } from '@oasis-sh/ui';
-import StyledMarkdown from '@parser/markdown/StyledMarkdown';
+import { StyledMarkdown } from '@oasis-sh/parser';
 import { GetServerSideProps } from 'next';
 import { SEO } from '@shared/SEO';
 import Link from 'next/link';
