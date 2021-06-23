622461dd78c306a485b4609e0edba76c489851d2
Bereket Semagn <semagnbereket@gmail.com>
2021-06-23 18:59:31 -0400

    chore(global): fix lint errors and lint

--- packages/ui/src/icons/arrows/SmallDownArrow.tsx
+++ packages/ui/src/icons/arrows/SmallDownArrow.tsx
@@ -4,19 +4,23 @@ export const SmallDownArrow: React.FC<React.SVGProps<SVGSVGElement>> = (
   props
 ) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg"
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 512 512"
-
       stroke="currentColor"
-      {...props}>
-      <polyline points="112 184 256 328 400 184"
-        style={{ fill: "none", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "48px" }} />
+      {...props}
+    >
+      <polyline
+        points="112 184 256 328 400 184"
+        style={{
+          fill: 'none',
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeWidth: '48px',
+        }}
+      />
     </svg>
-
-
-
-
   );
 };
--- packages/ui/src/icons/arrows/UpArrow.tsx
+++ packages/ui/src/icons/arrows/UpArrow.tsx
@@ -7,19 +7,20 @@ export const UpArrow: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
       width="24"
       height="24"
       viewBox="0 0 512 512"
-
       stroke="currentColor"
-      {...props}>
+      {...props}
+    >
       <polyline
         points="112 244 256 100 400 244"
-        style={{ fill: "none ", strokeWidth: "48px" }} />
+        style={{ fill: 'none ', strokeWidth: '48px' }}
+      />
       <line
         x1="256"
         y1="120"
         x2="256"
         y2="412"
-        style={{ fill: "none ", strokeWidth: "48px" }} />
+        style={{ fill: 'none ', strokeWidth: '48px' }}
+      />
     </svg>
-
   );
 };
