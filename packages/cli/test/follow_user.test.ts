5f447621973f9f46a5d555fca408380084c24621
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-23 22:33:57 +0800

    test(cli): todo tests dont accept functions
    i also ignored vim sessions

--- packages/cli/test/follow_user.test.ts
+++ packages/cli/test/follow_user.test.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 describe('following a user', () => {
   it.todo('applies changes');
   it.todo('rejects non existant user');
   it.todo('rejects unauthorized requests');
   it.todo('dumps raw json');
 });
-=======
-describe("following a user", () => {
-	it.todo("applies changes", () => {})
-	it.todo("rejects non existant user", () => {})
-	it.todo("rejects unauthorized requests")
-	it.todo("dumps raw json", () => {})
-})
->>>>>>> 45f35956 (feat(cli): implemented framework for tests)
--- packages/cli/test/search.test.ts
+++ packages/cli/test/search.test.ts
@@ -1,12 +1,5 @@
 describe('searching', () => {
-<<<<<<< HEAD
-  it.todo('yields expected results');
+ it.todo('yields expected results');
   it.todo('validates input');
   it.todo('dumps the raw json');
 });
-=======
-	it.todo("yields expected results", () => {})
-	it.todo('validates input', () => {})
-	it.todo('dumps the raw json', () => {})
-})
->>>>>>> 45f35956 (feat(cli): implemented framework for tests)
