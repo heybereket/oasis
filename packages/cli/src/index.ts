8eb2984987a54351f09d153c1920d08d76a012c3
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-23 22:21:43 +0800

    feat(cli): error out when unknown command is called

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,18 @@
 import * as yargs from 'yargs';
+import fs from 'fs';
+import * as log from './utils/log';
 
-yargs.commandDir('commands').demandCommand().version('1.0.0').argv;
+const argv = <any>(
+  yargs.commandDir('commands').demandCommand().version('1.0.0').argv
+);
+
+const commands = fs.readdirSync('./');
+
+for (const file in commands) {
+  if (!argv._) break;
+  const command = file.slice(0, -3);
+  if (argv._[0] !== command) {
+    log.error('that command does not exist!');
+    break;
+  }
+}
