a11b81a25e77801b76087f42a5553ffbf3d2e476
Simplyalex99 <42325851+Simplyalex99@users.noreply.github.com>
2021-06-23 18:48:20 -0400

    fix: updated arrow icons to ionicons icons (#445)
    * fix: safari flex gap
    
    * fix: renamed token field
    
    * feat: added new route for spotify
    
    * feat: added connection creater
    
    * fix: added shorthand object notation
    
    * Update createConnection.ts
    
    * fix: updated old arrow icons
    
    Co-authored-by: Bereket Semagn <semagnbereket@gmail.com>

--- packages/ui/src/icons/arrows/DownArrow.tsx
+++ packages/ui/src/icons/arrows/DownArrow.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 
 export const DownArrow: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
-    <svg
+
+
+    <svg xmlns="http://www.w3.org/2000/svg"
       width="20"
       height="20"
-      viewBox="0 0 14 8"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M1 1L7 7L13 1"
-        stroke="white"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+      viewBox="0 0 512 512"
+
+      stroke="currentColor"
+      {...props}>
+      <polyline
+        points="112 268 256 412 400 268"
+        style={{ fill: "none ", strokeWidth: "48px" }}
+      />
+      <line
+        x1="256"
+        y1="392"
+        x2="256"
+        y2="100"
+        style={{ fill: "none ", strokeWidth: "48px" }}
       />
     </svg>
   );
