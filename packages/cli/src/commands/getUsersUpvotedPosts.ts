df670321f1976ff403f7a339810f9810cc96d98a
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 21:08:26 +0800

    feat(cli): give getUsersUpvotedPosts.ts the works

--- packages/cli/src/commands/getUsersUpvotedPosts.ts
+++ packages/cli/src/commands/getUsersUpvotedPosts.ts
@@ -2,14 +2,44 @@ import * as log from '@oasis-sh/shared';
 import { gqlURL } from '@oasis-sh/shared';
 import { gql, GraphQLClient } from 'graphql-request';
 
-export const handler = async (yargs: any) => {
+interface GetUsersUpvotedPostsArguments {
+  username?: string;
+  limit: number;
+  offset: number;
+  json: boolean;
+}
+
+export const command =
+  'get_users_upvoted_posts <username> [limit] [offset] [json]';
+export const desc = "Fetches a user's upvoted posts. Returns an array of posts";
+
+export const builder = {
+  username: {
+    type: 'string',
+    describe: 'the user to query about',
+  },
+  limit: {
+    default: 10.0,
+    describe: 'sets limit on the number of posts to query',
+  },
+  offset: {
+    default: 0.0,
+    describe: 'offset the posts are being queried, useful for paginating posts',
+  },
+  json: {
+    describe:
+      'writes the raw JSON to stdout, powerful when used with jq (a JSON processor)',
+  },
+};
+
+export const handler = async (yargs: GetUsersUpvotedPostsArguments) => {
   const useJSON = yargs.json;
 
   const client = new GraphQLClient(gqlURL);
 
-  const username = yargs._[1];
-  const postsLimit = yargs.limit ?? 10.0;
-  const postsOffset = yargs.limit ?? 0.0;
+  const username = yargs.username;
+  const postsLimit = yargs.limit;
+  const postsOffset = yargs.limit;
 
   if (!username)
     return log.error('you need to pass <username> in order for this to work');
