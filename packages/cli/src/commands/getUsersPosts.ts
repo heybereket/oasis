2fc06b378ebf4e0c1f4de55a4d188a9e6946eb67
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 20:10:21 +0800

    feat(cli): document parameters for getUsersPosts.ts
    this completely borks the tests, i'll fix them later
    
    BREAKING CHANGE: the tests will no longer work, i am currently working on a fix

--- packages/cli/src/commands/getUsersPosts.ts
+++ packages/cli/src/commands/getUsersPosts.ts
@@ -2,14 +2,31 @@ import * as log from '@oasis-sh/shared';
 import { gqlURL } from '@oasis-sh/shared';
 import { gql, GraphQLClient } from 'graphql-request';
 
+export const command = 'get_users_posts <username> [json]';
+export const builder = {
+  limit: {
+    default: 10.0,
+    type: { type: 'float' },
+    describe: 'sets limit on the number of posts to query',
+  },
+  offset: {
+    default: 0.0,
+    describe:
+      'offsets the posts that are being queries, useful for paginating posts',
+  },
+  json: {
+    describe:
+      'writes the raw JSON to stdout, powerful when used with jq (a JSON processor)',
+  },
+};
 export const handler = async (yargs: any) => {
   const useJSON = yargs.json;
 
   const client = new GraphQLClient(gqlURL);
 
   const username = yargs._[1];
-  const postsLimit = yargs.limit ?? 10.0;
-  const postsOffset = yargs.offset ?? 0.0;
+  const postsLimit = yargs.limit;
+  const postsOffset = yargs.offset;
 
   if (!username)
     return log.error('you need to pass <username> in order for this to work');
