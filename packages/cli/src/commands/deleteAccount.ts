6db5c818883ade578ec15d72bdd889a6bba276e7
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 16:31:04 +0800

    refactor(cli): remove unused variables

--- packages/cli/src/commands/deleteAccount.ts
+++ packages/cli/src/commands/deleteAccount.ts
@@ -27,6 +27,6 @@
 
 import * as log from '@oasis-sh/shared';
 
-export const handler = async (yargs: any) => {
+export const handler = async () => {
   log.error('this command has not been implemented yet.');
 };
