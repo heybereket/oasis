e51b615dbb64ecbafe7ecb329ed7390104c63cff
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 21:23:09 +0800

    test(cli): ignore helper files

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -4,5 +4,6 @@ export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: false,
     rootDir: './__tests__',
+    testPathIgnorePatterns: ['./__tests__/schemas/*', './__tests__/helper.ts'],
   };
 };
