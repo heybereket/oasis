f4405b8c4705073200ed37033d997390fdf11492
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 21:01:14 +0800

    feat(cli): use types in follow.ts
    i don't know if this works as the actual implementation for authentication with cli is not done yet.

--- packages/cli/src/commands/follow.ts
+++ packages/cli/src/commands/follow.ts
@@ -2,36 +2,46 @@ import * as log from '@oasis-sh/shared';
 import { gqlURL } from '@oasis-sh/shared';
 import { gql, GraphQLClient } from 'graphql-request';
 
+interface FollowUserArguments {
+  user: string;
+  json: boolean;
+}
+
 export const command = 'follow <user> [json]';
 export const desc =
   'Follows a user. <user> must be a valid user ID. Must be authenticated with "oasis login" before running';
 
 export const builder = {
+  user: {
+    type: 'string',
+    describe: 'a valid user ID to follow',
+  },
   json: {
+    type: 'boolean',
     describe:
       'writes the raw JSON to stdout, powerful when used with jq (a JSON processor)',
   },
 };
 
-export const handler = async (yargs: any) => {
+export const handler = async (yargs: FollowUserArguments) => {
   const useJSON = yargs.json;
 
   const client = new GraphQLClient(gqlURL, {
     headers: { authorization: 'Bearer INSERT TOKEN HERE' },
   });
 
-  const userID = yargs._[1];
+  const user = yargs.user;
 
-  if (!userID)
+  if (!user)
     return log.error('you need to pass <username> in order for this to work');
 
   const query = gql`
-    mutation FollowUser($userId: String!) {
-      followUser(userId: $userId)
+    mutation FollowUser($user: String!) {
+      followUser(userId: $user)
     }
   `;
 
-  client.request(query, { userId: userID }).then((res) => {
+  client.request(query, { user }).then((res) => {
     if (useJSON) return console.log(JSON.stringify(res));
     log.info(res);
   });
