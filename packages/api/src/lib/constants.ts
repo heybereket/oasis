48e7f9b942ffabc2ed685b4eee246f1c1a95cdd9
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 16:14:27 +0800

    chore(cli): add testing scripts to package.json

--- packages/api/src/lib/constants.ts
+++ packages/api/src/lib/constants.ts
@@ -9,8 +9,7 @@ export const rateLimitTime = 60 * 60;
 // Environment Variables
 export const isProduction = process.env.NODE_ENV === 'production';
 export const isDevelopment = process.env.NODE_ENV !== 'production';
-export const sessionSecret =
-  process.env.SESSION_SECRET || 'oasis_session';
+export const sessionSecret = process.env.SESSION_SECRET || 'oasis_session';
 
 // Regex Patterns
 export const usernameRegex = /^[a-zA-Z0-9_.-]{3,15}$/;
