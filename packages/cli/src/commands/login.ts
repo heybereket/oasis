005fe6b41767213c1c19634aedd100972b34252c
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-28 16:39:11 +0800

    feat(cli): bundle cli so it can be used easily

---
+++ packages/cli/src/commands/login.ts
@@ -0,0 +1,5 @@
+import * as log from '@oasis-sh/shared';
+
+export const handler = async () => {
+	log.error('this command has not been implemented yet')
+}
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,17 +2,21 @@ import * as yargs from 'yargs';
 import fs from 'fs';
 import * as log from '@oasis-sh/shared';
 
-const argv = <any>(
-  yargs.commandDir('commands').demandCommand().version('1.0.0').argv
-);
+const main = () => {
+  const argv = <any>(
+    yargs.commandDir('commands').demandCommand().version('1.0.0').argv
+  );
 
-const commands = fs.readdirSync('./');
+  const commands = fs.readdirSync('./');
 
-for (const file in commands) {
-  if (!argv._) break;
-  const command = file.slice(0, -3);
-  if (argv._[0] !== command) {
-    log.error('that command does not exist!');
-    break;
+  for (const file in commands) {
+    if (!argv._) break;
+    const command = file.slice(0, -3);
+    if (argv._[0] !== command) {
+      log.error('that command does not exist!');
+      break;
+    }
   }
-}
+};
+
+export default main;
