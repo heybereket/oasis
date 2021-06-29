f30b8b0e394aa2e7e2ac4f480ef769d2141d9461
Bereket Semagn <semagnbereket@gmail.com>
2021-06-29 19:54:23 -0400

    chore(ui): change hastag icon props method

--- packages/ui/src/icons/channel/Hashtag.tsx
+++ packages/ui/src/icons/channel/Hashtag.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Hashtag: React.FC = (props: React.SVGProps<SVGSVGElement>) => {
+export const Hashtag: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
