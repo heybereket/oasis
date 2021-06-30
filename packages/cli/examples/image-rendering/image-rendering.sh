5d2bb98dd01d568e59ad2b4d14725139e510e731
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-06-30 09:04:32 +0800

    refactor(cli): add vertical padding along the y axis in image rendering

--- packages/cli/examples/image-rendering/image-rendering.sh
+++ packages/cli/examples/image-rendering/image-rendering.sh
@@ -9,7 +9,7 @@ IMAGE=$(../../bin/oasis get_post_by_id $1 --json | jq -r '.imageName')
 
 render_image() {
 	{
-    declare -Ap add_command=([action]="add" [identifier]="example1" [x]="10" [y]="0" [path]="/tmp/$1.jpg")
+    declare -Ap add_command=([action]="add" [identifier]="example1" [x]="10" [y]="10" [path]="/tmp/$1.jpg")
     sleep 120
 	} | ueberzug layer --parser bash
 }
