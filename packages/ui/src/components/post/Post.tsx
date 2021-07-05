1dce14687c78f7224aa72d0640324f55037eaa41
TheHackerCoding <29006605+TheHackerCoding@users.noreply.github.com>
2021-07-05 17:25:26 -0400

    fix(ui): i added a condition for when there's only one reply in a post (#475)
    * fix(ui/components/post/post.tsx): i added a condition for when there's only one reply
    
    When there's one reply to a post, say '1 reply' otherwise do whatever
    
    * style(ui/components/post/post.tsx): shortened the if statement

--- packages/ui/src/components/post/Post.tsx
+++ packages/ui/src/components/post/Post.tsx
@@ -212,7 +212,7 @@ export const Post: React.FC<Props> = ({
           <p className="text-sm font-medium">{date}</p>
           <CustomLink href={'/post/' + postData.id}>
             <div className="flex items-center space-x-2">
-              <p className="text-sm">{postData.comments.total} replies</p>
+              <p className="text-sm">{(postData.comments.total == 1) ? '1 reply' : postData.comments.total}</p>
               <Comments />
             </div>
           </CustomLink>
