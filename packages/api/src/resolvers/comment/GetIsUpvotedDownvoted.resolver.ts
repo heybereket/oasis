7a17e494373040e40e8006e2d453768a5a91011c
Bedanta Mukhopadhay <ongshu777@gmail.com>
2021-07-10 08:12:25 +0100

    refactor(api): Rename GetIsLikedDisliked to GetIsUpvotedDownvoted

---
+++ packages/api/src/resolvers/comment/GetIsUpvotedDownvoted.resolver.ts
@@ -0,0 +1,44 @@
+import Comment from '@entities/Comment';
+import { ContextType } from '@root/server';
+import { Ctx, FieldResolver, Resolver, Root } from 'type-graphql';
+
+@Resolver(() => Comment)
+export default class GetIsUpvotedDownvotedResolver {
+  @FieldResolver(() => Boolean)
+  async isUpvoted(
+    @Root() comment: Comment,
+    @Ctx() { getUser, hasAuth }: ContextType
+  ) {
+    if (hasAuth) {
+      const user = await getUser();
+      let retValue = false;
+      (await user.upvotedComments).forEach((res) => {
+        if (res.id === comment.id) {
+          retValue = true;
+        }
+      });
+      return retValue;
+    } else {
+      return false;
+    }
+  }
+
+  @FieldResolver(() => Boolean)
+  async isDownvoted(
+    @Root() comment: Comment,
+    @Ctx() { getUser, hasAuth }: ContextType
+  ) {
+    if (hasAuth) {
+      const user = await getUser();
+      let retValue = false;
+      (await user.downvotedComments).forEach((res) => {
+        if (res.id === comment.id) {
+          retValue = true;
+        }
+      });
+      return retValue;
+    } else {
+      return false;
+    }
+  }
+}
